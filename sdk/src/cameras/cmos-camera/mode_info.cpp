64216cc4028ebe1f30b25e73a674220c63d49702
Dan Nechita <dan.nechita@analog.com>
2021-02-10 17:21:30 +0200

    cmos: Add structure about mode info
    Signed-off-by: Dan Nechita <dan.nechita@analog.com>

---
+++ sdk/src/cameras/cmos-camera/mode_info.cpp
@@ -0,0 +1,41 @@
+/********************************************************************************/
+/*                                                                              */
+/* Copyright (c) 2020 Analog Devices, Inc. All Rights Reserved. */
+/* This software is proprietary to Analog Devices, Inc. and its licensors. */
+/*                                                                              */
+/********************************************************************************/
+#include "mode_info.h"
+#include <iostream>
+
+// TODO: load this information from camera module EEPROM
+ModeInfo::modeInfo ModeInfo::g_modeInfoData[] = {
+    {0, 512, 512, 6, 1317, 896},     {1, 320, 288, 9, 2162, 288},
+    {2, 512, 512, 3, 1317, 448},     {3, 1024, 1024, 1, 12289, 64},
+    {4, 640, 576, 10, 4321, 640},    {5, 1024, 1024, 10, 12289, 640},
+    {6, 1024, 256, 10, 2240, 878},   {7, 512, 512, 10, 2195, 896},
+    {8, 1024, 1024, 1, 12289, 64},   {9, 512, 512, 3, 1317, 448},
+    {10, 1024, 1024, 9, 12289, 576},
+};
+
+//Define the static Singleton pointer
+ModeInfo *ModeInfo::m_instance = NULL;
+
+ModeInfo *ModeInfo::getInstance() {
+    if (m_instance == NULL) {
+        m_instance = new ModeInfo();
+    }
+    return (m_instance);
+}
+
+ModeInfo::modeInfo ModeInfo::getModeInfo(unsigned int mode) {
+    if (0 <= mode && mode < getNumModes()) {
+        return ModeInfo::g_modeInfoData[mode];
+    }
+
+    return {0};
+}
+
+unsigned int ModeInfo::getNumModes() {
+    return sizeof(ModeInfo::g_modeInfoData) /
+           sizeof(ModeInfo::g_modeInfoData[0]);
+}
---
+++ sdk/src/cameras/cmos-camera/mode_info.h
@@ -0,0 +1,65 @@
+/********************************************************************************/
+/*                                                                              */
+/* Copyright (c) 2020 Analog Devices, Inc. All Rights Reserved.                 */
+/* This software is proprietary to Analog Devices, Inc. and its licensors.      */
+/*                                                                              */
+/********************************************************************************/
+#ifndef MODE_INFO
+#define MODE_INFO
+
+#include <aditof/status_definitions.h>
+#include <cstdint>
+
+/**
+ * @class ModeInfo
+ * @brief Singleton definition of Mode details (width, height, total captures, embedded_width, embedded_height)
+ * @note If low level USB objects or camera objects needs to get information of particular mode, they can use the single instance to get mode information
+ */
+class ModeInfo {
+
+  public:
+    /**
+    * @struct modeInfo
+    * @brief Maintains the information of all supported modes
+    */
+    typedef struct {
+        uint8_t mode;
+        uint16_t width;
+        uint16_t height;
+        uint8_t subframes;
+        uint16_t embed_width;
+        uint16_t embed_height;
+    } modeInfo;
+
+    /**
+     * @brief Get the single instance of the modeinfo
+     * @return ModeInfo*
+     * @see ModeInfo
+     * @note If low level USB objects or camera objects needs to get information of particular mode, they can use the single instance to get mode information
+     */
+    static ModeInfo *getInstance();
+
+    /**
+     * @brief Get the modeinformation based on mode
+     * param[in] mode - Mode for which the information is needed
+     * @return modeInfo*
+     * @see modeInfo
+     */
+    modeInfo getModeInfo(unsigned int mode);
+
+    /**
+     * @brief Get the number of modes supported
+     * @return int
+     */
+    unsigned int getNumModes();
+
+  private:
+    static modeInfo g_modeInfoData[]; //static array of all modes supported
+    static ModeInfo *m_instance;      //single instance
+    ModeInfo() {
+    } // private so that it can not be called, always access through getInstance()
+    ModeInfo(const ModeInfo &);            // copy constructor is private
+    ModeInfo &operator=(const ModeInfo &); // assignment operator is private
+};
+
+#endif // MODE_INFO
