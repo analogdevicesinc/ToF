35abcd32b35b41bddb19867bf50976a099e41c06
Virgil Litan <virgil.litan@analog.com>
2021-05-17 14:50:01 +0300

    fix frameType declaration for mode 5
    Signed-off-by: Virgil Litan <virgil.litan@analog.com>

--- sdk/src/connections/offline/offline_sensor_enumerator.cpp
+++ sdk/src/connections/offline/offline_sensor_enumerator.cpp
@@ -2,7 +2,8 @@
 #include "offline_depth_sensor.h"
 
 OfflineSensorEnumerator::OfflineSensorEnumerator() {
-    m_sensorsInfo.emplace_back(std::string(std::string(PROJECT_DIR) + "/local_ir_img.bin"));
+ //   m_sensorsInfo.emplace_back(std::string(std::string(PROJECT_DIR) + "/mode5_raw.bin"));
+    m_sensorsInfo.emplace_back(std::string(std::string(PROJECT_DIR) + "/mode3_raw.bin"));
 }
 
 aditof::Status OfflineSensorEnumerator::getDepthSensors(std::vector<std::shared_ptr<aditof::DepthSensorInterface> > &depthSensors) {
