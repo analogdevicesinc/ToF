dcc262bcee39a25049b259aa26d1180362d48258
Virgil Litan <virgil.litan@analog.com>
2021-05-18 14:08:41 +0300

    remove allocFrame from impl
    Signed-off-by: Virgil Litan <virgil.litan@analog.com>

--- sdk/src/frame.cpp
+++ sdk/src/frame.cpp
@@ -85,11 +85,5 @@ Status Frame::getAttribute(const std::string &attribute,
     return m_impl->getAttribute(attribute, value);
 }
 
-Status Frame::allocFrameData() {
-    FrameDetails details;
-    m_impl->getDetails(details);
-    m_impl->allocFrameData(details);
-    return aditof::Status::OK;
-}
 
 } // namespace aditof
