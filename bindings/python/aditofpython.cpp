72b7bf6537e4a950c91272d264ddd6ff95d215d7
Bogdan Togorean <bogdan.togorean@analog.com>
2021-06-29 16:22:32 +0300

    bindings: python: Add helper functions for register read/write
    Add regwrite, regread and regwriteburst Python functions
    
    Signed-off-by: Bogdan Togorean <bogdan.togorean@analog.com>

--- bindings/python/aditofpython.cpp
+++ bindings/python/aditofpython.cpp
@@ -299,6 +299,37 @@ PYBIND11_MODULE(aditofpython, m) {
                  return device.getFrame(ptr);
              },
              py::arg("buffer"))
+        .def("regread",
+             [](aditof::DepthSensorInterface &device,
+                uint16_t address) {
+                  uint16_t addrPtr[1], dataPtr[1];
+                  addrPtr[0] = address;
+                  device.readAfeRegisters(addrPtr, dataPtr, 1);
+                  return dataPtr[0];
+             },
+             py::arg("address"))
+        .def("regwrite",
+             [](aditof::DepthSensorInterface &device,
+                uint16_t address, uint16_t data) {
+                   uint16_t addrPtr[1], dataPtr[1];
+                   addrPtr[0] = address;
+                   dataPtr[0] = data;
+                   return device.writeAfeRegisters(addrPtr, dataPtr, 1);
+             },
+             py::arg("address"), py::arg("data"))
+        .def("regwriteburst",
+             [](aditof::DepthSensorInterface &device,
+                uint16_t address, py::array_t<uint16_t> data,
+                const std::string &incr) {
+                  uint16_t addrPtr[1];
+                  addrPtr[0] = address;
+                  if (incr.compare("increment") == 0)
+                    addrPtr[0] |= 0x4000;
+                  py::buffer_info dataBuffInfo = data.request();
+                  uint16_t *dataPtr = static_cast<uint16_t *>(dataBuffInfo.ptr);
+                  return device.writeAfeRegisters(addrPtr, dataPtr, data.size());
+             },
+             py::arg("address"), py::arg("data"), py::arg("increment"))
         .def("readAfeRegisters",
              [](aditof::DepthSensorInterface &device,
                 py::array_t<uint16_t> address, py::array_t<uint16_t> data,
