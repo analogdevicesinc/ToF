b9a65d07ac5e07280a23ae38bcfdd58614d3bfb4
Dan Nechita <dan.nechita@analog.com>
2021-07-09 08:50:30 +0300

    make_installer: update names, paths
    Signed-off-by: Dan Nechita <dan.nechita@analog.com>

--- ci/appveyor/make_installer.bat
+++ ci/appveyor/make_installer.bat
@@ -1,3 +1,3 @@
 set PATH=%PATH%;"C:\Program Files (x86)\Inno Setup 5"
-ISCC C:\projects\aditof-sdk\build\aditof-setup.iss
-appveyor PushArtifact C:\aditof-setup.exe
+ISCC C:\projects\aditof-sdk-rework\build\aditof-setup.iss
+appveyor PushArtifact C:\ADI-ITOF-Setup.exe
