7e38f56eecb8e6d38ab3bfaf4139ea93e62bd7c6
SeptimiuVana <Vana.Septimiu@analog.com>
2021-07-15 14:47:03 +0300

    bindings/python: update first-frame example
    Signed-off-by: SeptimiuVana <Vana.Septimiu@analog.com>

--- bindings/python/examples/first_frame/first_frame.py
+++ bindings/python/examples/first_frame/first_frame.py
@@ -43,11 +43,6 @@ camera1 = cameras[0]
 status = camera1.initialize()
 print("camera1.initialize()", status)
 
-modes = []
-status = camera1.getAvailableModes(modes)
-print("camera1.getAvailableModes()", status)
-print(modes)
-
 types = []
 status = camera1.getAvailableFrameTypes(types)
 print("camera1.getAvailableFrameTypes()", status)
@@ -58,12 +53,12 @@ status = camera1.getDetails(camDetails)
 print("camera1.getDetails()", status)
 print("camera1 details:", "id:", camDetails.cameraId, "connection:", camDetails.connection)
 
-status = camera1.setFrameType(types[0])
+status = camera1.setFrameType("mp_pcm")
 print("camera1.setFrameType()", status)
-print(types[0])
+print("mp_pcm")
 
-status = camera1.setMode(modes[0])
-print("camera1.setMode()", status)
+status = camera1.start()
+print("camera1.start()", status)
 
 frame = tof.Frame()
 status = camera1.requestFrame(frame)
