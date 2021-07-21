bd0214e00f84fdf48b7f66d126dee3b11090eca4
Dan Nechita <dan.nechita@analog.com>
2021-07-21 23:41:40 +0300

    ci/windows: update repo name in make_zip.bat
    Signed-off-by: Dan Nechita <dan.nechita@analog.com>

--- ci/appveyor/make_zip.bat
+++ ci/appveyor/make_zip.bat
@@ -1,5 +1,5 @@
-mkdir C:\aditof-sdk\
-pushd C:\aditof-sdk
+mkdir C:\aditof-sdk-rework\
+pushd C:\aditof-sdk-rework
 
 if "%APPVEYOR_BUILD_WORKER_IMAGE%"=="Visual Studio 2015" (
     set folder=vs14
@@ -20,38 +20,38 @@ mkdir x32\%folder%\bin
 mkdir x32\%folder%\bin\python
 mkdir x32\%folder%\lib
 
-copy C:\projects\aditof-sdk\build\sdk\Release\aditof.dll C:\aditof-sdk\x64\%folder%\bin
-copy C:\projects\aditof-sdk\build\sdk\Release\aditof.lib C:\aditof-sdk\x64\%folder%\lib
+copy C:\projects\aditof-sdk-rework\build\sdk\Release\aditof.dll C:\aditof-sdk-rework\x64\%folder%\bin
+copy C:\projects\aditof-sdk-rework\build\sdk\Release\aditof.lib C:\aditof-sdk-rework\x64\%folder%\lib
 
-copy "C:\projects\aditof-sdk\build\bindings\python\Release\*.pyd" C:\aditof-sdk\x64\%folder%\bin\python
-
-
-ren "C:\projects\aditof-sdk\build_debug\sdk\Debug\aditof.dll" "aditofd.dll"
-ren "C:\projects\aditof-sdk\build_debug\sdk\Debug\aditof.lib" "aditofd.lib"
-ren "C:\projects\aditof-sdk\build_debug\sdk\Debug\aditof.pdb" "aditofd.pdb"
+copy "C:\projects\aditof-sdk-rework\build\bindings\python\Release\*.pyd" C:\aditof-sdk-rework\x64\%folder%\bin\python
 
 
-copy C:\projects\aditof-sdk\build_debug\sdk\Debug\aditofd.dll C:\aditof-sdk\x64\%folder%\bin
-copy C:\projects\aditof-sdk\build_debug\sdk\Debug\aditofd.pdb C:\aditof-sdk\x64\%folder%\bin
-copy C:\projects\aditof-sdk\build_debug\sdk\Debug\aditofd.lib C:\aditof-sdk\x64\%folder%\lib
-copy "C:\projects\aditof-sdk\build_debug\bindings\python\Debug\*.pyd" C:\aditof-sdk\x64\%folder%\bin\python
+ren "C:\projects\aditof-sdk-rework\build_debug\sdk\Debug\aditof.dll" "aditofd.dll"
+ren "C:\projects\aditof-sdk-rework\build_debug\sdk\Debug\aditof.lib" "aditofd.lib"
+ren "C:\projects\aditof-sdk-rework\build_debug\sdk\Debug\aditof.pdb" "aditofd.pdb"
 
-copy C:\projects\aditof-sdk\build_x32\sdk\Release\aditof.dll C:\aditof-sdk\x32\%folder%\bin
-copy C:\projects\aditof-sdk\build_x32\sdk\Release\aditof.lib C:\aditof-sdk\x32\%folder%\lib
-copy "C:\projects\aditof-sdk\build_x32\bindings\python\Release\*.pyd" C:\aditof-sdk\x32\%folder%\bin\python
 
-ren "C:\projects\aditof-sdk\build_debug_x32\sdk\Debug\aditof.dll" "aditofd.dll"
-ren "C:\projects\aditof-sdk\build_debug_x32\sdk\Debug\aditof.lib" "aditofd.lib"
-ren "C:\projects\aditof-sdk\build_debug_x32\sdk\Debug\aditof.pdb" "aditofd.pdb"
+copy C:\projects\aditof-sdk-rework\build_debug\sdk\Debug\aditofd.dll C:\aditof-sdk-rework\x64\%folder%\bin
+copy C:\projects\aditof-sdk-rework\build_debug\sdk\Debug\aditofd.pdb C:\aditof-sdk-rework\x64\%folder%\bin
+copy C:\projects\aditof-sdk-rework\build_debug\sdk\Debug\aditofd.lib C:\aditof-sdk-rework\x64\%folder%\lib
+copy "C:\projects\aditof-sdk-rework\build_debug\bindings\python\Debug\*.pyd" C:\aditof-sdk-rework\x64\%folder%\bin\python
 
-copy C:\projects\aditof-sdk\build_debug_x32\sdk\Debug\aditofd.dll C:\aditof-sdk\x32\%folder%\bin
-copy C:\projects\aditof-sdk\build_debug_x32\sdk\Debug\aditofd.pdb C:\aditof-sdk\x32\%folder%\bin
-copy C:\projects\aditof-sdk\build_debug_x32\sdk\Debug\aditofd.lib C:\aditof-sdk\x32\%folder%\lib
-copy "C:\projects\aditof-sdk\build_debug_x32\bindings\python\Debug\*.pyd" C:\aditof-sdk\x32\%folder%\bin\python
+copy C:\projects\aditof-sdk-rework\build_x32\sdk\Release\aditof.dll C:\aditof-sdk-rework\x32\%folder%\bin
+copy C:\projects\aditof-sdk-rework\build_x32\sdk\Release\aditof.lib C:\aditof-sdk-rework\x32\%folder%\lib
+copy "C:\projects\aditof-sdk-rework\build_x32\bindings\python\Release\*.pyd" C:\aditof-sdk-rework\x32\%folder%\bin\python
 
-xcopy /E C:\projects\aditof-sdk\sdk\include C:\aditof-sdk\include
+ren "C:\projects\aditof-sdk-rework\build_debug_x32\sdk\Debug\aditof.dll" "aditofd.dll"
+ren "C:\projects\aditof-sdk-rework\build_debug_x32\sdk\Debug\aditof.lib" "aditofd.lib"
+ren "C:\projects\aditof-sdk-rework\build_debug_x32\sdk\Debug\aditof.pdb" "aditofd.pdb"
 
-7z a "C:\aditof-sdk-%folder%.zip" C:\aditof-sdk
-appveyor PushArtifact C:\aditof-sdk-%folder%.zip
+copy C:\projects\aditof-sdk-rework\build_debug_x32\sdk\Debug\aditofd.dll C:\aditof-sdk-rework\x32\%folder%\bin
+copy C:\projects\aditof-sdk-rework\build_debug_x32\sdk\Debug\aditofd.pdb C:\aditof-sdk-rework\x32\%folder%\bin
+copy C:\projects\aditof-sdk-rework\build_debug_x32\sdk\Debug\aditofd.lib C:\aditof-sdk-rework\x32\%folder%\lib
+copy "C:\projects\aditof-sdk-rework\build_debug_x32\bindings\python\Debug\*.pyd" C:\aditof-sdk-rework\x32\%folder%\bin\python
+
+xcopy /E C:\projects\aditof-sdk-rework\sdk\include C:\aditof-sdk-rework\include
+
+7z a "C:\aditof-sdk-rework-%folder%.zip" C:\aditof-sdk-rework
+appveyor PushArtifact C:\aditof-sdk-rework-%folder%.zip
 
 popd
