f245f3704a0e57658784c337061a536be7edc248
Dan Nechita <dan.nechita@analog.com>
2021-11-11 12:58:02 +0200

    examples/aditof-demo: Fix memory leak by learing queue with frames
    What was happening was that after clicking the stop button and then
    click start again, the memory usage (on Windows) was increasing by 1.4
    GB. And this could go on for each stop->start iteration.
    The fix is to clear the queue with captured frames when stopping the
    stream capture. Otherwise the next captuing will start with a large
    queue (thinking it's empty) and appending new frames to that.
    
    Signed-off-by: Dan Nechita <dan.nechita@analog.com>

--- examples/aditof-demo/aditofdemocontroller.cpp
+++ examples/aditof-demo/aditofdemocontroller.cpp
@@ -168,6 +168,7 @@ void AdiTofDemoController::stopCapture() {
     if (m_workerThread.joinable()) {
         m_workerThread.join();
     }
+    m_queue.clear();
 }
 
 std::string AdiTofDemoController::getMode() const {
