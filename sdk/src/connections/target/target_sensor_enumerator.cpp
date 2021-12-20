f63b78a00898fbb183ef422e3034f43fcc5dd313
SeptimiuVana <Vana.Septimiu@analog.com>
2021-12-20 16:49:19 +0200

    sdk: implement getName function to parse sensor name over network
    Signed-off-by: SeptimiuVana <Vana.Septimiu@analog.com>

--- sdk/src/connections/target/target_sensor_enumerator.cpp
+++ sdk/src/connections/target/target_sensor_enumerator.cpp
@@ -31,8 +31,8 @@
  */
 #include "connections/target/target_sensor_enumerator.h"
 #include "connections/target/adsd3100_sensor.h"
-#include "connections/target/pulsatrix_sensor.h"
 #include "connections/target/eeprom.h"
+#include "connections/target/pulsatrix_sensor.h"
 
 using namespace aditof;
 
@@ -43,14 +43,14 @@ Status TargetSensorEnumerator::getDepthSensors(
 
     for (const auto &sInfo : m_sensorsInfo) {
         switch (sInfo.sensorType) {
-        case SensorType::SENSOR_ADSD3100: {
-            auto sensor = std::make_shared<Adsd3100Sensor>(
+        case SensorType::SENSOR_PULSATRIX: {
+            auto sensor = std::make_shared<PulsatrixSensor>(
                 sInfo.driverPath, sInfo.subDevPath, sInfo.captureDev);
             depthSensors.emplace_back(sensor);
             break;
         }
-        case SensorType::SENSOR_PULSATRIX: {
-            auto sensor = std::make_shared<PulsatrixSensor>(
+        case SensorType::SENSOR_ADSD3100: {
+            auto sensor = std::make_shared<Adsd3100Sensor>(
                 sInfo.driverPath, sInfo.subDevPath, sInfo.captureDev);
             depthSensors.emplace_back(sensor);
             break;
