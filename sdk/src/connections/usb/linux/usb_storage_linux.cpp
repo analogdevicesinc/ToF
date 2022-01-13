551036dfe68288c070a50331d6de869c961bd5ee
SeptimiuVana <Vana.Septimiu@analog.com>
2022-01-13 16:49:00 +0200

    sdk/src/usb/linux/usb_storage_linux.cpp: update storage functions to work with new uvc
    Signed-off-by: SeptimiuVana <Vana.Septimiu@analog.com>

--- sdk/src/connections/usb/linux/usb_storage_linux.cpp
+++ sdk/src/connections/usb/linux/usb_storage_linux.cpp
@@ -29,6 +29,7 @@
  * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
+#include "usb_buffer.pb.h"
 #include "connections/usb/usb_storage.h"
 #include "usb_linux_utils.h"
 #include "utils.h"
@@ -62,7 +63,42 @@ Status UsbStorage::open(void *handle) {
 
     m_implData->fd = *(reinterpret_cast<int *>(handle));
 
-    return Status::OK;
+    using namespace aditof;
+    Status status = Status::OK;
+
+    // Construct request message
+    usb_payload::ClientRequest requestMsg;
+    requestMsg.set_func_name(usb_payload::FunctionName::STORAGE_OPEN);
+
+    // Send request
+    std::string requestStr;
+    requestMsg.SerializeToString(&requestStr);
+    status = UsbLinuxUtils::uvcExUnitSendRequest(m_implData->fd, requestStr);
+    if (status != aditof::Status::OK) {
+        LOG(ERROR) << "Request open flash storage failed";
+        return status;
+    }
+
+    // Read UVC gadget response
+    std::string responseStr;
+    status = UsbLinuxUtils::uvcExUnitGetResponse(m_implData->fd, responseStr);
+    if (status != aditof::Status::OK) {
+        LOG(ERROR) << "Failed to get response of the request to open flash memory";
+        return status;
+    }
+    usb_payload::ServerResponse responseMsg;
+    bool parsed = responseMsg.ParseFromString(responseStr);
+    if (!parsed) {
+        LOG(ERROR) << "Failed to deserialize string containing UVC gadget response";
+        return aditof::Status::INVALID_ARGUMENT;
+    }
+
+    if (responseMsg.status() != usb_payload::Status::OK) {
+        LOG(ERROR) << "Open flash memory operation failed on UVC gadget";
+        return static_cast<aditof::Status>(responseMsg.status());
+    }
+
+    return status;
 }
 
 Status UsbStorage::read(const uint32_t address, uint8_t *data,
@@ -76,14 +112,44 @@ Status UsbStorage::read(const uint32_t address, uint8_t *data,
         return Status::INVALID_ARGUMENT;
     }
 
-    int ret = UsbLinuxUtils::uvcExUnitReadBuffer(
-        m_implData->fd, 5, m_implData->id, address, data, bytesCount);
-    if (ret < 0) {
-        LOG(WARNING)
-            << "Failed to read buffer through UVC extension unit. Error: "
-            << ret;
-        return Status::GENERIC_ERROR;
+    aditof::Status status = aditof::Status::OK;
+
+    usb_payload::ClientRequest requestMsg;
+    requestMsg.set_func_name(usb_payload::FunctionName::STORAGE_READ);
+    requestMsg.add_func_int32_param(static_cast<::google::int32>(bytesCount));
+    requestMsg.add_func_int32_param(static_cast<::google::int32>(address));
+    requestMsg.add_func_bytes_param(data, bytesCount);
+
+    // Send request
+    std::string requestStr;
+    requestMsg.SerializeToString(&requestStr);
+    status = UsbLinuxUtils::uvcExUnitSendRequest(m_implData->fd, requestStr);
+    if (status != aditof::Status::OK) {
+        LOG(ERROR) << "Request to read memory failed";
+        return status;
     }
+
+    // Read UVC gadget response
+    std::string responseStr;
+    status = UsbLinuxUtils::uvcExUnitGetResponse(m_implData->fd, responseStr);
+    if (status != aditof::Status::OK) {
+        LOG(ERROR) << "Failed to get response of the request to read memory";
+        return status;
+    }
+    usb_payload::ServerResponse responseMsg;
+    bool parsed = responseMsg.ParseFromString(responseStr);
+    if (!parsed) {
+        LOG(ERROR) << "Failed to deserialize string containing UVC gadget response";
+        return aditof::Status::INVALID_ARGUMENT;
+    }
+
+    if (responseMsg.status() != usb_payload::Status::OK) {
+        LOG(ERROR) << "Read registers operation failed on UVC gadget";
+        return static_cast<aditof::Status>(responseMsg.status());
+    }
+   
+    memcpy(data, responseMsg.bytes_payload(0).c_str(), responseMsg.bytes_payload(0).length());
+
     return Status::OK;
 }
 
@@ -99,38 +165,43 @@ Status UsbStorage::write(const uint32_t address, const uint8_t *data,
         return Status::INVALID_ARGUMENT;
     }
 
-    int ret = UsbLinuxUtils::uvcExUnitWriteBuffer(
-        m_implData->fd, 6, m_implData->id, address, data, bytesCount);
-    if (ret < 0) {
-        LOG(WARNING)
-            << "Failed to write buffer through UVC extension unit. Error: "
-            << ret;
-        return Status::GENERIC_ERROR;
+    Status status = Status::OK;
+
+    // TO DO: is it required to call UvcFindNodeAndGetControl here?
+
+    // Construct request message
+    usb_payload::ClientRequest requestMsg;
+    requestMsg.set_func_name(usb_payload::FunctionName::STORAGE_WRITE);
+    requestMsg.add_func_int32_param(static_cast<::google::int32>(bytesCount));
+    requestMsg.add_func_int32_param(static_cast<::google::int32>(address));
+    requestMsg.add_func_bytes_param(data, bytesCount);
+
+    // Send request
+    std::string requestStr;
+    requestMsg.SerializeToString(&requestStr);
+    status = UsbLinuxUtils::uvcExUnitSendRequest(m_implData->fd, requestStr);
+    if (status != aditof::Status::OK) {
+        LOG(ERROR) << "Request to write registers failed";
+        return status;
     }
 
-    uint8_t eepromWriteStatus = 0;
-    int attempts = 300;
-
-    while (eepromWriteStatus == 0 && attempts > 0) {
-        ret = UsbLinuxUtils::uvcExUnitReadOnePacket(
-            m_implData->fd, 7, nullptr, 0, &eepromWriteStatus, 1, 1);
-        --attempts;
-        if (ret < 0) {
-            LOG(WARNING)
-                << "Failed to read a packet via UVC extension unit. Error: "
-                << ret;
-            return Status::GENERIC_ERROR;
-        }
-
-        if (!eepromWriteStatus) {
-            std::this_thread::sleep_for(std::chrono::milliseconds(100));
-        }
+    // Read UVC gadget response
+    std::string responseStr;
+    status = UsbLinuxUtils::uvcExUnitGetResponse(m_implData->fd, responseStr);
+    if (status != aditof::Status::OK) {
+        LOG(ERROR) << "Failed to get response of the request to write registers";
+        return status;
+    }
+    usb_payload::ServerResponse responseMsg;
+    bool parsed = responseMsg.ParseFromString(responseStr);
+    if (!parsed) {
+        LOG(ERROR) << "Failed to deserialize string containing UVC gadget response";
+        return aditof::Status::INVALID_ARGUMENT;
     }
 
-    if (attempts == 0 && eepromWriteStatus == 0) {
-        LOG(WARNING) << "Write operation failed. Target is in a state where "
-                        "EEPROM write operations cannot be done.";
-        return Status::GENERIC_ERROR;
+    if (responseMsg.status() != usb_payload::Status::OK) {
+        LOG(ERROR) << "Read registers operation failed on UVC gadget";
+        return static_cast<aditof::Status>(responseMsg.status());
     }
 
     return Status::OK;
@@ -138,11 +209,92 @@ Status UsbStorage::write(const uint32_t address, const uint8_t *data,
 
 Status UsbStorage::getCapacity(size_t &nbBytes) const
 {
-    // TO DO: implement
-    return aditof::Status::UNAVAILABLE;
+    if (!m_implData->fd) {
+        LOG(ERROR) << "Cannot read! EEPROM is not opened.";
+        return Status::GENERIC_ERROR;
+    }
+
+    using namespace aditof;
+
+    Status status = Status::OK;
+
+    // TO DO: is it required to call UvcFindNodeAndGetControl here?
+
+    // Construct request message
+    usb_payload::ClientRequest requestMsg;
+    requestMsg.set_func_name(usb_payload::FunctionName::STORAGE_READ_CAPACITY);
+    requestMsg.add_func_int32_param(static_cast<::google::int32>(nbBytes));
+
+    // Send request
+    std::string requestStr;
+    requestMsg.SerializeToString(&requestStr);
+    status = UsbLinuxUtils::uvcExUnitSendRequest(m_implData->fd, requestStr);
+    if (status != aditof::Status::OK) {
+        LOG(ERROR) << "Request to read memory size failed";
+        return status;
+    }
+
+    // Read UVC gadget response
+    std::string responseStr;
+    status = UsbLinuxUtils::uvcExUnitGetResponse(m_implData->fd, responseStr);
+    if (status != aditof::Status::OK) {
+        LOG(ERROR) << "Failed to get response of the request to read memory size";
+        return status;
+    }
+    usb_payload::ServerResponse responseMsg;
+    bool parsed = responseMsg.ParseFromString(responseStr);
+    if (!parsed) {
+        LOG(ERROR) << "Failed to deserialize string containing UVC gadget response";
+        return aditof::Status::INVALID_ARGUMENT;
+    }
+
+    if (responseMsg.status() != usb_payload::Status::OK) {
+        LOG(ERROR) << "Read memory size operation failed on UVC gadget";
+        return static_cast<aditof::Status>(responseMsg.status());
+    }
+   
+    nbBytes = responseMsg.int32_payload(0);
+
+    return Status::OK;
 }
 
 Status UsbStorage::close() {
+    using namespace aditof;
+
+    Status status = Status::OK;
+
+    // Construct request message
+    usb_payload::ClientRequest requestMsg;
+    requestMsg.set_func_name(usb_payload::FunctionName::STORAGE_CLOSE);
+
+        // Send request
+    std::string requestStr;
+    requestMsg.SerializeToString(&requestStr);
+    status = UsbLinuxUtils::uvcExUnitSendRequest(m_implData->fd, requestStr);
+    if (status != aditof::Status::OK) {
+        LOG(ERROR) << "Request close flash storage failed";
+        return status;
+    }
+
+    // Read UVC gadget response
+    std::string responseStr;
+    status = UsbLinuxUtils::uvcExUnitGetResponse(m_implData->fd, responseStr);
+    if (status != aditof::Status::OK) {
+        LOG(ERROR) << "Failed to get response of the request to close flash memory";
+        return status;
+    }
+    usb_payload::ServerResponse responseMsg;
+    bool parsed = responseMsg.ParseFromString(responseStr);
+    if (!parsed) {
+        LOG(ERROR) << "Failed to deserialize string containing UVC gadget response";
+        return aditof::Status::INVALID_ARGUMENT;
+    }
+
+    if (responseMsg.status() != usb_payload::Status::OK) {
+        LOG(ERROR) << "Close flash memory operation failed on UVC gadget";
+        return static_cast<aditof::Status>(responseMsg.status());
+    }
+
     m_implData->fd = -1;
 
     return Status::OK;
