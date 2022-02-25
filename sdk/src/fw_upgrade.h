f2cbf0378700aeb3add78d433ec70296c33a7f2a
Jimmy Ye <Jimmy.Ye@analog.com>
2022-02-25 16:10:09 -0500

    The initial program to do FW upgrade, need to integrate with Dan's SDK interface

---
+++ sdk/src/fw_upgrade.h
@@ -0,0 +1,10 @@
+#ifndef FW_UPGRADE_H
+#define FW_UPGRADE_H
+
+//#include <../include/aditof/status_definitions.h>
+
+#include <cstdint>
+#include <string>
+int pulsa_fw_upgrade(std::string filepath);
+
+#endif
