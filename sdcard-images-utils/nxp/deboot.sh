9f43ceb12766d74e6327ae1e07772ff05c9bf5ff
Bogdan Togorean <bogdan.togorean@analog.com>
2022-03-04 09:26:33 +0200

    sdcard-images-utils: deboot.sh: Install htpdate bs and ssh
    Signed-off-by: Bogdan Togorean <bogdan.togorean@analog.com>

--- sdcard-images-utils/nxp/deboot.sh
+++ sdcard-images-utils/nxp/deboot.sh
@@ -67,7 +67,7 @@ function setup_config() {
   TZ_CITY=UTC
   LOCALE_LANG=en_US.UTF-8
   ADD_LIST='git build-essential gcc autoconf nano vim'
-  ADD_LIST_ST_3='i2c-tools v4l-utils rfkill wpasupplicant libtool libconfig-dev avahi-daemon'
+  ADD_LIST_ST_3='i2c-tools v4l-utils rfkill wpasupplicant libtool libconfig-dev avahi-daemon htpdate openssh-server bc'
 
   # output example of the config file
 #  cat <<EOF>config-example
