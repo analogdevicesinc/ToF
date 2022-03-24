d52b49b00a6f9112635c7cf079511a2a0a1ad879
Timotei Molcut <timotei.molcut@analog.com>
2022-03-24 12:48:54 +0200

    changed apparitions of 'aditof-sdk-rework' to 'ToF'
    Signed-off-by: Timotei Molcut <timotei.molcut@analog.com>

--- ci/appveyor/make_installer.bat
+++ ci/appveyor/make_installer.bat
@@ -1,3 +1,3 @@
 set PATH=%PATH%;"C:\Program Files (x86)\Inno Setup 5"
-ISCC C:\projects\aditof-sdk-rework\build\aditof-setup.iss
+ISCC C:\projects\ToF\build\aditof-setup.iss
 appveyor PushArtifact C:\ADI-ITOF-Setup.exe
