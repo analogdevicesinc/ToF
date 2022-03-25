eb8114230a75d328f377dbabbc2faf63c6aff145
SeptimiuVana <Vana.Septimiu@analog.com>
2022-03-25 15:58:55 +0200

    sdk/common/adi/depth-compute-stubs: update stubs to match depth compute v3.0.0
    Signed-off-by: SeptimiuVana <Vana.Septimiu@analog.com>

--- sdk/common/adi/depth-compute-stub/src/tofiConfig.cpp
+++ sdk/common/adi/depth-compute-stub/src/tofiConfig.cpp
@@ -24,6 +24,33 @@ TofiConfig *InitTofiConfig(ConfigFileData *p_cal_file_data,
     Obj->p_tofi_config_str=0;
     Obj->xyz_table = *xyzObj;
     return Obj;
-                           };
+    };
+
+TofiConfig *InitTofiConfig_isp(ConfigFileData *p_ini_file_data,
+                               uint16_t mode, uint32_t *p_status,
+                               TofiXYZDealiasData *p_xyz_dealias_data){
+    XYZTable *xyzObj;
+    xyzObj->p_x_table = 0;
+    xyzObj->p_y_table = 0;
+    xyzObj->p_z_table = 0;
+    
+    TofiConfig *Obj;
+    Obj->n_cols=0;
+    Obj->n_rows=0;
+    Obj->p_cal_gain_block=0;
+    Obj->p_cal_reg_block=0;
+    Obj->p_camera_intrinsics=0;
+    Obj->p_lsdac_block=0;
+    Obj->p_tofi_cal_config=0;
+    Obj->p_tofi_config_str=0;
+    Obj->xyz_table = *xyzObj;
+    return Obj;
+    };
+
+uint32_t GetXYZ_DealiasData(ConfigFileData *ccb_data,
+                            TofiXYZDealiasData *p_xyz_data){
+    return 0;
+    };
+
 
 void FreeTofiConfig(TofiConfig *p_tofi_cal_config) {};
