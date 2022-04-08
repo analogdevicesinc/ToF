c7f7901fe7390f671b092f5ced5d82643e060095
Bogdan Togorean <bogdan.togorean@analog.com>
2022-04-08 14:42:21 +0300

    UVC fixes for ADSD3500
    Signed-off-by: Bogdan Togorean <bogdan.togorean@analog.com>

--- apps/uvc-app/tof-sdk-interface.h
+++ apps/uvc-app/tof-sdk-interface.h
@@ -7,7 +7,10 @@
 #define EXTERNC
 #endif
 
+#include <stdio.h>
+
 EXTERNC int init_tof_sdk(char* cap_dev_path);
-EXTERNC const char* handleClientRequest(const char *buf);
+EXTERNC void handleClientRequest(const char *in_buf, const size_t in_len, char **out_buf, size_t *out_len);
 
 #endif
+
