06faa34dc9adac4477ea66f7cdce07135561aa54
Bogdan Togorean <bogdan.togorean@analog.com>
2022-05-24 10:02:01 +0300

    sdcard-image-utils: patches: linux-imx: Fix MP frames width
    The width and height of MP frame needs to be reversed as the superframes
    have width=1024 and height=3*1024
    
    Signed-off-by: Bogdan Togorean <bogdan.togorean@analog.com>

--- sdk/src/connections/target/adsd3500_sensor.h
+++ sdk/src/connections/target/adsd3500_sensor.h
@@ -129,14 +129,14 @@ class Adsd3500Sensor : public aditof::DepthSensorInterface,
     },
     {"mp", 
          {
-             {"raw", 3072, 1024},
+             {"raw", 1024, 3072},
              {"ir", 1024, 1024},
              {"xyz", 1024, 1024},
              {"depth", 1024, 1024},
              {"embedded_header", 1, 128}
          },
-         3072,
          1024,
+         3072,
     }
     };
 };
