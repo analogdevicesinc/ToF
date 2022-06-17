12bb8a4bbcf0cf237c39ddc226be1ed3daf24457
cristina-suteu <cristina.suteu@analog.com>
2022-06-17 15:46:07 +0300

    added API to read sw versions
    Signed-off-by: cristina-suteu <cristina.suteu@analog.com>

--- sdk/src/connections/usb/linux/usb_sensor_enumerator.cpp
+++ sdk/src/connections/usb/linux/usb_sensor_enumerator.cpp
@@ -245,3 +245,27 @@ Status UsbSensorEnumerator::getTemperatureSensors(
 
     return Status::OK;
 }
+
+aditof::Status UsbSensorEnumerator::getUbootVersion(
+    std::string &uBootVersion) const {
+    uBootVersion = m_uBootVersion;
+    return aditof::Status::OK;
+}
+
+aditof::Status UsbSensorEnumerator::getKernelVersion(
+    std::string &kernelVersion) const {
+    kernelVersion = m_kernelVersion;
+    return aditof::Status::OK;
+}
+
+aditof::Status UsbSensorEnumerator::getRfsVersion(
+    std::string &rfsVersion) const {
+    rfsVersion = m_rfsVersion;
+    return aditof::Status::OK;
+}
+
+aditof::Status UsbSensorEnumerator::getSdkVersion(
+    std::string &sdkVersion) const {
+    sdkVersion = m_sdkVersion;
+    return aditof::Status::OK;
+}
