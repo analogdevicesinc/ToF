6b042362072ba3840d5940646508f382c118e930
SeptimiuVana <Vana.Septimiu@analog.com>
2022-06-29 10:29:24 +0300

    sdk/src/connections/adsd3500_sensor: minor build fixes
    Signed-off-by: SeptimiuVana <Vana.Septimiu@analog.com>

--- sdk/src/connections/target/adsd3500_sensor.h
+++ sdk/src/connections/target/adsd3500_sensor.h
@@ -75,7 +75,7 @@ class Adsd3500Sensor : public aditof::DepthSensorInterface,
     virtual aditof::Status adsd3500_read_cmd(uint16_t cmd, uint16_t *data) override;
     virtual aditof::Status adsd3500_write_cmd(uint16_t cmd, uint16_t data) override;
     virtual aditof::Status adsd3500_read_payload_cmd(uint32_t cmd, uint8_t* readback_data, uint16_t payload_len) override;
-    virtual aditof::Status adsd3500_read_payload(uint8_t* payload, uint32_t payload_len) override;
+    virtual aditof::Status adsd3500_read_payload(uint8_t* payload, uint16_t payload_len) override;
     virtual aditof::Status adsd3500_write_payload_cmd(uint32_t cmd, uint8_t* payload, uint16_t payload_len) override;
     virtual aditof::Status adsd3500_write_payload(uint8_t* payload, uint16_t payload_len) override;
 
