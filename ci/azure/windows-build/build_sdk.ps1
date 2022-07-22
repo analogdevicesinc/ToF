fe5ee71af38fe06ace4dd094617dbecb14faeeda
SeptimiuVana <Vana.Septimiu@analog.com>
2022-07-22 13:45:24 +0300

    ci/azure: create libs folder and add missing NXP flags
    Signed-off-by: SeptimiuVana <Vana.Septimiu@analog.com>

--- ci/azure/windows-build/build_sdk.ps1
+++ ci/azure/windows-build/build_sdk.ps1
@@ -4,6 +4,7 @@ cinst opencv --version 3.4.1
 #build sdk
 mkdir build_Release
 mkdir build_Debug
+mkdir ../libs
 
 cd build_Release
 cmake -DUSE_DEPTH_COMPUTE_STUBS=1 -DWITH_OPENCV=on -DWITH_PYTHON=on -DCMAKE_PREFIX_PATH="../deps_installed/Release/glog;../deps_installed/Release/protobuf;../deps_installed/Release/websockets;..\deps_installed\OpenSSL-Win64" -DOpenCV_DIR="C:/tools/opencv/build/x64/vc15/lib" -DOPENSSL_INCLUDE_DIRS="..\deps_installed\OpenSSL-Win64\include" ..
