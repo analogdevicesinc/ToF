5efaa4dda6bb592f5a81b99f13a20f8aec09f69d
SeptimiuVana <Vana.Septimiu@analog.com>
2022-09-06 12:00:56 +0300

    sdk/src/buffer_processor: implement destructor and setVideoProperties function
    Signed-off-by: SeptimiuVana <Vana.Septimiu@analog.com>

--- sdk/src/connections/target/buffer_processor.cpp
+++ sdk/src/connections/target/buffer_processor.cpp
@@ -47,29 +47,73 @@ static int xioctl(int fh, unsigned int request, void *arg) {
 
 BufferProcessor::BufferProcessor() : {
     m_outputFrameWitdh(0), m_outputFrameHeight(0), m_tofiConfig(nullptr),
-        m_tofiComputeContext(nullptr) {}
+        m_tofiComputeContext(nullptr), m_vidPropSet(false),
+        m_processorPropSet(false), m_fd(0)
+}
 
-~BufferProcessor::BufferProcessor() {}
+~BufferProcessor::BufferProcessor() {
+    if (m_fd != 0) {
+        if (::close(m_fd) == -1) {
+            LOG(ERROR) << "Failed to close " << m_videoDevice
+                       << " error: " << strerror(errno);
+        }
+    }
+}
 
-aditof::Status BufferProcessor::open(){
+aditof::Status BufferProcessor::open() {
     using namespace aditof;
     Status status = Status::OK;
 
     m_fd = ::open(m_videoDevice, O_RDWR);
     if (m_fd == -1) {
-        LOG(WARNING) << "Cannot open " << OUTPUT_DEVICE << "errno: " << errno
-                     << "error: " << strerror(errno);
+        LOG(ERROR) << "Cannot open " << OUTPUT_DEVICE << "errno: " << errno
+                   << "error: " << strerror(errno);
         return Status::GENERIC_ERROR;
     }
 
-    if(xioctl(m_fd, VIDIOC_QUERYCAP, &m_videoCapabilities) == -1)
-        LOG(WARNING) << devName << " VIDIOC_QUERYCAP error";
+    if (xioctl(m_fd, VIDIOC_QUERYCAP, &m_videoCapabilities) == -1) {
+        LOG(ERROR) << devName << " VIDIOC_QUERYCAP error";
         return Status::GENERIC_ERROR;
     }
 
-	memset(&m_videoFormat, 0, sizeof(m_videoFormat));
-    if(xioctl(m_fd, VIDIOC_G_FMT, &m_videoFormat) == -1)
-        LOG(WARNING) << devName << " VIDIOC_G_FMT error";
+    memset(&m_videoFormat, 0, sizeof(m_videoFormat));
+    if (xioctl(m_fd, VIDIOC_G_FMT, &m_videoFormat) == -1) {
+        LOG(ERROR) << devName << " VIDIOC_G_FMT error";
         return Status::GENERIC_ERROR;
     }
+
+    return status;
 }
+
+aditof::Status BufferProcessor::setVideoProperties(int frameWidth,
+                                                   int frameHeight) {
+    using namespace aditof;
+    Status status = Status::OK;
+
+    m_outputFrameWidth = frameWidth;
+    m_outputFrameHeight = frameHeight;
+
+    m_videoFormat.type = V4L2_BUF_TYPE_VIDEO_OUTPUT;
+    m_videoFormat.fmt.pix.width = frameWidth;
+    m_videoFormat.fmt.pix.height = frameHeight;
+    m_videoFormat.fmt.pix.pixelformat = V4L2_PIX_FMT_SBGGR12;
+    m_videoFormat.fmt.pix.sizeimage = frameWitdh * frameHeight;
+    m_videoFormat.fmt.pix.field = V4L2_FIELD_NONE;
+    m_videoFormat.fmt.pix.bytesperline = framewidth;
+    m_videoFormat.fmt.pix.colorspace = V4L2_COLORSPACE_SRGB;
+
+    if (xioctl(m_fd, VIDIOC_S_FMT, &m_videoFormat) == -1) {
+        LOG(ERROR) << "Failed to set format!";
+        return Status::GENERIC_ERROR;
+    }
+
+    return status;
+}
+
+aditof::Status BufferProcessor::setProcessorProperties(uint8_t *iniFile,
+                                                       uint16_t iniFileLength,
+                                                       uint8_t *calData,
+                                                       uint16_t calDataLength) {
+}
+
+aditof::Status BufferProcessor::processFrame(uint16_t *buffer = nullptr) {}
