6204912752fcb6fe4779ffebf427163d91478f2d
rbudai <robert.budai@analog.com>
2022-09-15 13:33:16 +0300

    ROS2: added initial version
    Signed-off-by: rbudai <robert.budai@analog.com>

---
+++ bindings/ros2/ros2_setup.bash
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+DISTRO_CODENAME=$( (lsb_release -sc || . /etc/os-release; echo ${VERSION_CODENAME} ) 2>/dev/null)
+
+#determine ROS distribution
+if [ $DISTRO_CODENAME = "bionic" ]
+then
+    ROS_DISTRO="crystal"
+    #     echo crystal
+    
+elif [ $DISTRO_CODENAME = "focal" ]
+then
+    ROS_DISTRO="foxy"
+    #     echo foxy
+    
+elif [ $DISTRO_CODENAME = "buster" ]
+then
+    ROS_DISTRO="humble"
+    #     echo humble
+fi
+
+mkdir -p ${COLCON_WS}/src
+cd ${COLCON_WS}/src
+ln -sf ${ROS2_SRC_DIR}/tof_ros2cpp/ .
+
+cd ${COLCON_WS}
+source /opt/ros/$ROS_DISTRO/setup.sh
+
+colcon build --cmake-args -DADITOF_CMAKE_INSTALL_PREFIX=$1 -DADITOF_CMAKE_PREFIX_PATH=$2 -DLIBTOFI_LIBDIR_PATH=$3
