ce67bc3f40454ce3d7db4304e4a80f83634ff5f2
SeptimiuVana <Vana.Septimiu@analog.com>
2022-09-29 15:35:21 +0300

    sdk/cameras/mode_info.cpp: add feature to switch between new and old modes
    Signed-off-by: SeptimiuVana <Vana.Septimiu@analog.com>

--- sdk/src/cameras/itof-camera/mode_info.cpp
+++ sdk/src/cameras/itof-camera/mode_info.cpp
@@ -10,14 +10,27 @@
 
 // TODO: load this information from camera module EEPROM
 #ifndef ADSD3030
-ModeInfo::modeInfo ModeInfo::g_modeInfoData[] = {
+//use new modes by default
+ModeInfo::modeInfo *ModeInfo::g_modeInfoData = ModeInfo::g_newModes;
+
+ModeInfo::modeInfo ModeInfo::g_oldModes[] = {
+    {1, 320, 288, 9, 2162, 288, 0},
+    {3, 1024, 1024, 1, 12289, 64, 1},
+    {7, 512, 512, 10, 2195, 896, 1},
+    {10, 1024, 1024, 9, 12289, 576, 0},
+};
+
+//TO DO: update table with new values
+ModeInfo::modeInfo ModeInfo::g_newModes[] = {
     {1, 320, 288, 9, 2162, 288, 0},
     {3, 1024, 1024, 1, 12289, 64, 1},
     {7, 512, 512, 10, 2195, 896, 1},
     {10, 1024, 1024, 9, 12289, 576, 0},
 };
 #else
-ModeInfo::modeInfo ModeInfo::g_modeInfoData[] = {
+ModeInfo::modeInfo *ModeInfo::g_modeInfoData = ModeInfo::g_adsd3030Modes;
+
+ModeInfo::modeInfo ModeInfo::g_adsd3030Modes[] = {
     {5, 512, 640, 10, 1670, 1472, 1},
 };
 #endif
@@ -51,16 +64,22 @@ aditof::Status convertCameraMode(const std::string &mode,
                                  uint8_t &convertedMode) {
 
 #ifndef ADSD3030
-    if (mode == "lt_bin") {
-        convertedMode = 1;
-    } else if (mode == "pcm") {
-        convertedMode = 3;
-    } else if (mode == "qmp") {
-        convertedMode = 7;
-    } else if (mode == "mp") {
-        convertedMode = 10;
+    int modeVersion = ModeInfo::getInstance()->getModeVersion();
+    if (modeVersion) {
+        //complete names for each mode
+        return aditof::Status::UNAVAILABLE;
     } else {
-        return aditof::Status::INVALID_ARGUMENT;
+        if (mode == "lt_bin") {
+            convertedMode = 1;
+        } else if (mode == "pcm") {
+            convertedMode = 3;
+        } else if (mode == "qmp") {
+            convertedMode = 7;
+        } else if (mode == "mp") {
+            convertedMode = 10;
+        } else {
+            return aditof::Status::INVALID_ARGUMENT;
+        }
     }
 #else
     if (mode == "vga") {
@@ -80,3 +99,27 @@ ModeInfo::modeInfo ModeInfo::getModeInfo(const std::string &mode) {
 
     return getModeInfo(modeIndex);
 }
+
+aditof::Status ModeInfo::setModeVersion(int version) {
+    using namespace aditof;
+    Status status = Status::OK;
+
+#ifndef ADSD3030
+    if (version) {
+        g_modeInfoData = g_newModes;
+    } else {
+        g_modeInfoData = g_oldModes;
+    }
+#endif
+
+    return status;
+};
+
+int ModeInfo::getModeVersion() {
+#ifndef ADSD3030
+    if (g_modeInfoData == g_newModes) {
+        return 1;
+    }
+#endif
+    return 0;
+};
