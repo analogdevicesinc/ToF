d2ce8f35d46e2cbc49a0054294f74b34b9516bfe
rbudai <robert.budai@analog.com>
2022-10-12 10:31:09 +0200

    ROS2: setup stepps ON nxp
    Signed-off-by: rbudai <robert.budai@analog.com>

---
+++ scripts/nxp/ros2_ws_setup.bash
@@ -0,0 +1,11 @@
+STARTING_PATH=${pwd}
+cd /home/${USER}
+wget https://swdownloads.analog.com/cse/aditof/tof_ros2_build_nxp/ros2_nxp_2022_10_12.tar
+tar -xvf ros2_nxp_2022_10_12.tar
+rm ros2_nxp_2022_10_12.tar
+cd ros2_foxy/src
+git clone https://github.com/rbudai98/tof_ros2cpp.git
+cd ..
+source install/local_setup.bash
+colcon build 
+cd ${STARTING_PATH}
