a9a4772586e5f10944d57cd4879c7c1ced329db9
Dan Nechita <dan.nechita@analog.com>
2022-11-01 12:50:37 +0200

    bindings/python: Add API to set some parameters for adsd3500
    Signed-off-by: Dan Nechita <dan.nechita@analog.com>

--- bindings/python/aditofpython.cpp
+++ bindings/python/aditofpython.cpp
@@ -280,7 +280,48 @@ PYBIND11_MODULE(aditofpython, m) {
             py::arg("tempSensors"))
         .def("adsd3500_set_toggle_mode",
              &aditof::Camera::adsd3500_set_toggle_mode, py::arg("mode"))
-        .def("adsd3500_toggle_fsync", &aditof::Camera::adsd3500_toggle_fsync);
+        .def("adsd3500_toggle_fsync", &aditof::Camera::adsd3500_toggle_fsync)
+        .def("adsd3500_toggle_fsync", &aditof::Camera::adsd3500_toggle_fsync)
+        .def("adsd3500SetABinvalidationThreshold",
+             &aditof::Camera::adsd3500SetABinvalidationThreshold,
+             py::arg("threshold"))
+        .def("adsd3500GetABinvalidationThreshold",
+             &aditof::Camera::adsd3500GetABinvalidationThreshold,
+             py::arg("threshold"))
+        .def("adsd3500SetConfidenceThreshold",
+             &aditof::Camera::adsd3500SetConfidenceThreshold,
+             py::arg("threshold"))
+        .def("adsd3500GetConfidenceThreshold",
+             &aditof::Camera::adsd3500GetConfidenceThreshold,
+             py::arg("threshold"))
+        .def("adsd3500SetConfidenceThreshold",
+             &aditof::Camera::adsd3500SetConfidenceThreshold,
+             py::arg("threshold"))
+        .def("adsd3500GetConfidenceThreshold",
+             &aditof::Camera::adsd3500GetConfidenceThreshold,
+             py::arg("threshold"))
+        .def("adsd3500SetJBLFfilterEnableState",
+             &aditof::Camera::adsd3500SetJBLFfilterEnableState,
+             py::arg("enable"))
+        .def("adsd3500GetJBLFfilterEnableState",
+             &aditof::Camera::adsd3500GetJBLFfilterEnableState,
+             py::arg("enabled"))
+        .def("adsd3500SetJBLFfilterSize",
+             &aditof::Camera::adsd3500SetJBLFfilterSize, py::arg("size"))
+        .def("adsd3500GetJBLFfilterSize",
+             &aditof::Camera::adsd3500GetJBLFfilterSize, py::arg("size"))
+        .def("adsd3500SetRadialThresholdMin",
+             &aditof::Camera::adsd3500SetRadialThresholdMin,
+             py::arg("threshold"))
+        .def("adsd3500GetRadialThresholdMin",
+             &aditof::Camera::adsd3500GetRadialThresholdMin,
+             py::arg("threshold"))
+        .def("adsd3500SetRadialThresholdMax",
+             &aditof::Camera::adsd3500SetRadialThresholdMax,
+             py::arg("threshold"))
+        .def("adsd3500GetRadialThresholdMax",
+             &aditof::Camera::adsd3500GetRadialThresholdMax,
+             py::arg("threshold"));
 
     // Frame
     py::class_<aditof::Frame>(m, "Frame")
