09054579553dc789912c1b5496e29815f10576a5
Alexandru Lie <Alexandru.Lie@analog.com>
2023-02-10 12:12:38 +0200

    changes for offline sensor in sdk
    Signed-off-by: Alexandru Lie <Alexandru.Lie@analog.com>

--- sdk/src/connections/offline/offline_depth_sensor.cpp
+++ sdk/src/connections/offline/offline_depth_sensor.cpp
@@ -1,23 +1,21 @@
 #include "offline_depth_sensor.h"
-#include <cstring>
-#include <fstream>
-#include <iostream>
-#include <dirent.h>
-#include <vector>
-#include <map>
-#include <iterator>
-#include <glog/logging.h>
 #include "tofi/tofi_config.h"
-
+#include <cstring>
+#include <dirent.h>
+#include <fstream>
+#include <glog/logging.h>
+#include <iostream>
+#include <iterator>
+#include <map>
+#include <vector>
 
 OfflineDepthSensor::OfflineDepthSensor(std::string path) : m_path(path) {
     m_sensorDetails.connectionType = aditof::ConnectionType::OFFLINE;
     m_connectionType = "offline";
-
 }
 
 OfflineDepthSensor::~OfflineDepthSensor() {
-    for(auto index : m_frameTypes){
+    for (auto index : m_frameTypes) {
         delete[] index.second.first;
     }
     m_frameTypes.clear();
@@ -25,18 +23,17 @@ OfflineDepthSensor::~OfflineDepthSensor() {
 
 aditof::Status OfflineDepthSensor::getFrame(uint16_t *buffer) {
     auto frame = m_frameTypes.find(m_frameTypeSelected + ".bin");
-    if(frame == m_frameTypes.end()){
-        LOG(WARNING)
-            <<"get frame error "
-            << m_frameTypeSelected << " is not found";
+    if (frame == m_frameTypes.end()) {
+        LOG(WARNING) << "get frame error " << m_frameTypeSelected
+                     << " is not found";
         return aditof::Status::GENERIC_ERROR;
     }
     memcpy(buffer, frame->second.first, frame->second.second);
-    return aditof::Status::OK;  
+    return aditof::Status::OK;
 }
 
 aditof::Status OfflineDepthSensor::open() {
-    std::vector<std::string>frameTypesResources;
+    std::vector<std::string> frameTypesResources;
     std::streampos size;
     std::string fileName;
     uint16_t *buffer;
@@ -44,23 +41,25 @@ aditof::Status OfflineDepthSensor::open() {
     DIR *dir = opendir(m_path.c_str());
     while ((entry = readdir(dir)) != NULL) {
         fileName = entry->d_name;
-        if(fileName != "." && fileName != "..")
+        if (fileName != "." && fileName != "..")
             frameTypesResources.push_back(fileName);
     }
 
-    for (int index = 0; index < frameTypesResources.size(); index++){
+    for (int index = 0; index < frameTypesResources.size(); index++) {
         std::ifstream ifs;
-        ifs.open(m_path + '/' + frameTypesResources[index], std::ios::in|std::ios::binary);
+        ifs.open(m_path + '/' + frameTypesResources[index],
+                 std::ios::in | std::ios::binary);
         std::string content((std::istreambuf_iterator<char>(ifs)),
                             (std::istreambuf_iterator<char>()));
         buffer = new uint16_t[content.size()];
         for (int i = 0; i < content.size(); i += 2) {
             buffer[i / 2] = *(uint16_t *)&content[i];
         }
-        m_frameTypes.insert({frameTypesResources[index], std::make_pair(buffer, content.size())});
+        m_frameTypes.insert({frameTypesResources[index],
+                             std::make_pair(buffer, content.size())});
         ifs.close();
     }
-    return aditof::Status::OK; 
+    return aditof::Status::OK;
 }
 
 aditof::Status OfflineDepthSensor::start() { return aditof::Status::OK; }
@@ -110,7 +109,7 @@ aditof::Status OfflineDepthSensor::setControl(const std::string &control,
 
 aditof::Status OfflineDepthSensor::getControl(const std::string &control,
                                               std::string &value) const {
-    if(control == "imagerType")
+    if (control == "imagerType")
         value = "1";
     else
         value = "2";
@@ -142,11 +141,11 @@ aditof::Status OfflineDepthSensor::adsd3500_write_cmd(uint16_t cmd,
                                                       uint16_t data) {
     return aditof::Status::OK;
 }
-          
+
 aditof::Status OfflineDepthSensor::adsd3500_read_payload_cmd(
     uint32_t cmd, uint8_t *readback_data, uint16_t payload_len) {
     TofiXYZDealiasData dealiasStruct;
-    if(cmd == 0x01){
+    if (cmd == 0x01) {
         dealiasStruct.camera_intrinsics.fx = 783.19696;
         dealiasStruct.camera_intrinsics.fy = 783.248596;
         dealiasStruct.camera_intrinsics.cx = 514.108887;
@@ -161,43 +160,44 @@ aditof::Status OfflineDepthSensor::adsd3500_read_payload_cmd(
         dealiasStruct.camera_intrinsics.k6 = 0.190698504;
         dealiasStruct.camera_intrinsics.p2 = 5.45154289e-05;
         dealiasStruct.camera_intrinsics.p1 = 0.000267527241;
-        memcpy(readback_data,&dealiasStruct.camera_intrinsics,sizeof(CameraIntrinsics));
-    }
-    else if(cmd == 0x02){
-        if(readback_data[0] == 0){ // daca 1024x1024
-            dealiasStruct.n_rows = 1024;
-            dealiasStruct.n_cols = 1024;
+        memcpy(readback_data, &dealiasStruct.camera_intrinsics,
+               sizeof(CameraIntrinsics));
+    } else if (cmd == 0x02) {
+        if (readback_data[0] == 3) { // daca 1024x1024
+            dealiasStruct.n_rows = 512;
+            dealiasStruct.n_cols = 512;
             dealiasStruct.row_bin_factor = 1;
             dealiasStruct.col_bin_factor = 1;
-        }
-        else if(readback_data[0] == 5){
+            dealiasStruct.n_sensor_rows = 1024;
+            dealiasStruct.n_sensor_cols = 1024;
+        } else if (readback_data[0] == 2) {
             dealiasStruct.n_rows = 512;
             dealiasStruct.n_cols = 512;
             dealiasStruct.row_bin_factor = 2;
             dealiasStruct.col_bin_factor = 2;
+            dealiasStruct.n_sensor_rows = 1024;
+            dealiasStruct.n_sensor_cols = 1024;
         }
         dealiasStruct.n_freqs = 2;
         dealiasStruct.n_offset_rows = 0;
         dealiasStruct.n_offset_cols = 0;
-        dealiasStruct.n_sensor_rows = 1024;
-        dealiasStruct.n_sensor_cols = 1024;
         dealiasStruct.FreqIndex[0] = 1;
         dealiasStruct.FreqIndex[1] = 2;
         dealiasStruct.FreqIndex[2] = 0;
         dealiasStruct.Freq[0] = 14200;
         dealiasStruct.Freq[1] = 17750;
         dealiasStruct.Freq[2] = 0;
-        memcpy(readback_data,&dealiasStruct,sizeof(TofiXYZDealiasData) - sizeof(CameraIntrinsics));
-    }
-    else{
-        memset(readback_data,1,payload_len);
+        memcpy(readback_data, &dealiasStruct,
+               sizeof(TofiXYZDealiasData) - sizeof(CameraIntrinsics));
+    } else {
+        memset(readback_data, 1, payload_len);
     }
     return aditof::Status::OK;
 }
 
 aditof::Status OfflineDepthSensor::adsd3500_read_payload(uint8_t *payload,
                                                          uint16_t payload_len) {
-                                                            
+
     return aditof::Status::OK;
 }
 
