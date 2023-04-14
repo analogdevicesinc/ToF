95afb79461335e369602e618b9ab701a70148006
rbudai <robert.budai@analog.com>
2023-04-14 16:12:32 +0300

    tools: tof lib generator update
    Library generator update for differents sensors in different directories
    
    Signed-off-by: rbudai <robert.budai@analog.com>

--- tools/tof_lib_gen/generate_dpkg.bash
+++ tools/tof_lib_gen/generate_dpkg.bash
@@ -1,6 +1,20 @@
+# Generating dpkg for Crosby
 echo "Generating dpkg for Ubuntu 18.04 Amd64"
-sudo DOCKER_BUILDKIT=1 docker build --file ubuntu_18_04/Dockerfile --output out_ubuntu18 .
+sudo DOCKER_BUILDKIT=1 docker build --file ubuntu_18_04/crosby/Dockerfile --output crosby/out_ubuntu18 .
 echo "Generating dpkg for Ubuntu 20.04 Amd64"
-sudo DOCKER_BUILDKIT=1 docker build --file ubuntu_20_04/Dockerfile --output out_ubuntu20 .
+sudo DOCKER_BUILDKIT=1 docker build --file ubuntu_20_04/crosby/Dockerfile --output crosby/out_ubuntu20 .
 echo "Generating dpkg for Ubuntu 22.04 Amd64"
-sudo DOCKER_BUILDKIT=1 docker build --file ubuntu_22_04/Dockerfile --output out_ubuntu22 .
+sudo DOCKER_BUILDKIT=1 docker build --file ubuntu_22_04/crosby/Dockerfile --output crosby/out_ubuntu22 .
+
+# enabling read and write 
+sudo chmod -R a+rx crosby
+
+# Generating dpkg for adsd3030 (adsd3030)
+echo "Generating dpkg for Ubuntu 18.04 Amd64"
+sudo DOCKER_BUILDKIT=1 docker build --file ubuntu_18_04/adsd3030/Dockerfile --output adsd3030/out_ubuntu18 .
+echo "Generating dpkg for Ubuntu 20.04 Amd64"
+sudo DOCKER_BUILDKIT=1 docker build --file ubuntu_20_04/adsd3030/Dockerfile --output adsd3030/out_ubuntu20 .
+echo "Generating dpkg for Ubuntu 22.04 Amd64"
+sudo DOCKER_BUILDKIT=1 docker build --file ubuntu_22_04/adsd3030/Dockerfile --output adsd3030/out_ubuntu22 .
+
+sudo chmod -R a+rx adsd3030
