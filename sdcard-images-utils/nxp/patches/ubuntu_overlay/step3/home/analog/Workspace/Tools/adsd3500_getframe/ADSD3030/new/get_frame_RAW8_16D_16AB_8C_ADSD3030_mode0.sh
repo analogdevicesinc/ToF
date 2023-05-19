f3f2062a94f44f0e210279745dafdf2841756f4d
Karnati <Naveen.Karnati@analog.com>
2023-05-19 19:47:53 +0530

    Updating NXP dependent scripts

--- sdcard-images-utils/nxp/patches/ubuntu_overlay/step3/home/analog/Workspace/Tools/adsd3500_getframe/ADSD3030/new/get_frame_RAW8_16D_16AB_8C_ADSD3030_mode0.sh
+++ sdcard-images-utils/nxp/patches/ubuntu_overlay/step3/home/analog/Workspace/Tools/adsd3500_getframe/ADSD3030/new/get_frame_RAW8_16D_16AB_8C_ADSD3030_mode0.sh
@@ -2,10 +2,10 @@
 
 nr_frames=${1:-1}
 
-v4l2-ctl --set-ctrl=operating_mode=5 -d /dev/v4l-subdev1
+v4l2-ctl --set-ctrl=operating_mode=0 -d /dev/v4l-subdev1
 v4l2-ctl --set-ctrl=phase_depth_bits=6 -d /dev/v4l-subdev1
-v4l2-ctl --set-ctrl=ab_bits=4 -d /dev/v4l-subdev1
+v4l2-ctl --set-ctrl=ab_bits=6 -d /dev/v4l-subdev1
 v4l2-ctl --set-ctrl=confidence_bits=2 -d /dev/v4l-subdev1
 v4l2-ctl --set-ctrl=ab_averaging=1 -d /dev/v4l-subdev1
 v4l2-ctl --set-ctrl=depth_enable=1 -d /dev/v4l-subdev1
-v4l2-ctl --device /dev/video0 --set-fmt-video=width=2048,height=640,pixelformat=BA81 --stream-mmap --stream-to=frame.bin --stream-count=$nr_frames
+v4l2-ctl --device /dev/video0 --set-fmt-video=width=2560,height=640,pixelformat=BA81 --stream-mmap --stream-to=mode0.bin --stream-count=$nr_frames
--- sdcard-images-utils/nxp/patches/ubuntu_overlay/step3/home/analog/Workspace/Tools/host_boot_tools/readme.txt
+++ sdcard-images-utils/nxp/patches/ubuntu_overlay/step3/home/analog/Workspace/Tools/host_boot_tools/readme.txt
@@ -4,6 +4,4 @@ ATTENTION! host_boot.py require direct access to ADSD3500 I2C device. Make sure 
 
 1.	The host_boot.py script loads the ADSD3500 boot firmware through I2C
         ADSD3500 contain a BootROM bootloader which makes possible this host-boot usecase
-2.	Run "python3 host_boot.py" for loading the default "walden_lite.stream" to address 0x38
-		Parameters are accepted and other .stream file or I2C address can be specifyed
-		"python3 host_boot.py walden_lite.stream 0x38"
+2. Run ./host_boot.sh host_boot.stream to host boot the connected device.
