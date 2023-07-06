7df0f52db446a2e67109faa8490cf446c6a38294
Karnati <Naveen.Karnati@analog.com>
2023-07-06 16:15:23 +0530

    Adding adi-backup.service to backup NVM and CCB on First boot of NXP
    Signed-off-by: Karnati <Naveen.Karnati@analog.com>

---
+++ sdcard-images-utils/nxp/patches/ubuntu_overlay/step1/usr/share/systemd/backup_nvm_ccb.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+nvm_size=741376
+sleep 15
+if [ -d "/boot" ]; then
+	if [ ! -f "/boot/nvm_backup.bin" ]; then
+		echo "Backup NVM_Flash image to /boot"
+		chmod +x /home/analog/Workspace/Tools/host_boot_tools/NVM_Flash_Utility/NVM_READ
+		/home/analog/Workspace/Tools/host_boot_tools/NVM_Flash_Utility/NVM_READ /boot/nvm_backup.bin
+	else
+		a=$(wc -c < "/boot/nvm_backup.bin")
+		if [ $a != $nvm_size ]; then
+			echo "Backup NVM_Flash image to /boot"
+			chmod +x /home/analog/Workspace/Tools/host_boot_tools/NVM_Flash_Utility/NVM_READ
+			/home/analog/Workspace/Tools/host_boot_tools/NVM_Flash_Utility/NVM_READ /boot/nvm_backup.bin
+		fi
+
+	fi
+	if [ ! -f "/boot/ccb_backup.ccb" ]; then
+		echo "Backup CCB to /boot"
+		chmod +x /home/analog/Workspace/Tools/host_boot_tools/NVM_Flash_Utility/CCB_READ
+		/home/analog/Workspace/Tools/host_boot_tools/NVM_Flash_Utility/CCB_READ /boot/ccb_backup.ccb
+	fi
+fi
