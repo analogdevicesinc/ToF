3b3637d06f81c085e6a876b97e9ebd4395ea7bd6
rbudai <robert.budai@analog.com>
2023-07-18 17:29:32 +0200

    tof_lib_gen: forced build from skratch and added cmake flags
    Signed-off-by: rbudai <robert.budai@analog.com>

--- tools/tof_lib_gen/generate_dpkg.bash
+++ tools/tof_lib_gen/generate_dpkg.bash
@@ -1,16 +1,16 @@
 # Generating dpkg for Crosby
 echo "Generating dpkg for Ubuntu 20.04 Amd64"
-sudo DOCKER_BUILDKIT=1 docker build --file ubuntu_20_04/crosby/Dockerfile --output crosby/out_ubuntu20 .
+sudo DOCKER_BUILDKIT=1 docker build --no-cache --file ubuntu_20_04/crosby/Dockerfile --output crosby/out_ubuntu20 .
 echo "Generating dpkg for Ubuntu 22.04 Amd64"
-sudo DOCKER_BUILDKIT=1 docker build --file ubuntu_22_04/crosby/Dockerfile --output crosby/out_ubuntu22 .
+sudo DOCKER_BUILDKIT=1 docker build --no-cache --file ubuntu_22_04/crosby/Dockerfile --output crosby/out_ubuntu22 .
 
 # enabling read and write 
 sudo chmod -R a+rx crosby
 
-# Generating dpkg for adsd3030 (adsd3030)
+# # Generating dpkg for adsd3030 (adsd3030)
 echo "Generating dpkg for Ubuntu 20.04 Amd64"
-sudo DOCKER_BUILDKIT=1 docker build --file ubuntu_20_04/adsd3030/Dockerfile --output adsd3030/out_ubuntu20 .
+sudo DOCKER_BUILDKIT=1 docker build --no-cache --file ubuntu_20_04/adsd3030/Dockerfile --output adsd3030/out_ubuntu20 .
 echo "Generating dpkg for Ubuntu 22.04 Amd64"
-sudo DOCKER_BUILDKIT=1 docker build --file ubuntu_22_04/adsd3030/Dockerfile --output adsd3030/out_ubuntu22 .
+sudo DOCKER_BUILDKIT=1 docker build --no-cache --file ubuntu_22_04/adsd3030/Dockerfile --output adsd3030/out_ubuntu22 .
 
 sudo chmod -R a+rx adsd3030
