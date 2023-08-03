793e85b9e7f250cff8a812e3a7e9f7c346aa4966
rbudai <robert.budai@analog.com>
2023-08-03 17:37:04 +0200

    apps/server: add DEPTH_COMPUTE condition on frame getting
    Signed-off-by: rbudai <robert.budai@analog.com>

--- sdk/src/cameras/itof-camera/camera_itof.cpp
+++ sdk/src/cameras/itof-camera/camera_itof.cpp
@@ -71,9 +71,9 @@ CameraItof::CameraItof(
       m_fsyncMode(-1), m_mipiOutputSpeed(-1), m_adsd3500ImagerType(0),
       m_modesVersion(0), m_enableTempCompenstation(-1),
       m_enableMetaDatainAB(-1), m_enableEdgeConfidence(-1),
-      m_targetFramesAreComputed(true) {
-#ifdef TARGET
-    m_targetFramesAreComputed = false;
+      m_targetFramesAreComputed(false) {
+#ifdef DEPTH_COMPUTE_ON_TARGET
+    m_targetFramesAreComputed = true;
 #endif
     FloatToLinGenerateTable();
     memset(&m_xyzTable, 0, sizeof(m_xyzTable));
