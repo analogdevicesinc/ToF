51bd64b4ca2d27568b6358e09bf4c64b6a755d01
SeptimiuVana <Vana.Septimiu@analog.com>
2023-11-03 13:42:29 +0200

    sdk/adsd_errs.h: temporary remove static variables
    Signed-off-by: SeptimiuVana <Vana.Septimiu@analog.com>

--- sdk/include/aditof/adsd_errs.h
+++ sdk/include/aditof/adsd_errs.h
@@ -46,7 +46,6 @@ namespace aditof {
  * Included with aditof/camera.h
  * For example: 
  *    ADSDErrors err;
- *    LOG(INFO) << ADSDErrors::ADSD3500_STATUS_INVALID_MODE;
  *    LOG(INFO) << err.GetStringADSD3500(0x0001);
  */
 class ADSDErrors {
@@ -169,204 +168,202 @@ class ADSDErrors {
     /**
      * @brief Mode selected is invalid.
      */
-    static const uint16_t ADSD3500_STATUS_INVALID_MODE = 0x0001;
+    const uint16_t ADSD3500_STATUS_INVALID_MODE = 0x0001;
     /**
      * @brief The JBLF filter size speficied is incorrect.
      */
-    static const uint16_t ADSD3500_STATUS_INVALID_JBLF_FILTER_SIZE = 0x0002;
+    const uint16_t ADSD3500_STATUS_INVALID_JBLF_FILTER_SIZE = 0x0002;
     /**
      * @brief An unsupported command was sent to the ASDD3500.
      */
-    static const uint16_t ADSD3500_STATUS_UNSUPPORTED_CMD = 0x0003;
+    const uint16_t ADSD3500_STATUS_UNSUPPORTED_CMD = 0x0003;
     /**
      * @brief TODO
      */
-    static const uint16_t ADSD3500_STATUS_INVALID_MEMORY_REGION = 0x0004;
+    const uint16_t ADSD3500_STATUS_INVALID_MEMORY_REGION = 0x0004;
     /**
      * @brief The ADSD3500 firmware CRC check failed.
      */
-    static const uint16_t ADSD3500_STATUS_INVALID_FIRMWARE_CRC = 0x0005;
+    const uint16_t ADSD3500_STATUS_INVALID_FIRMWARE_CRC = 0x0005;
     /**
      * @brief The imager firmware is not valid.
      */
-    static const uint16_t ADSD3500_STATUS_INVALID_IMAGER = 0x0006;
+    const uint16_t ADSD3500_STATUS_INVALID_IMAGER = 0x0006;
     /**
      * @brief The imager CCB file is not valid.
      */
-    static const uint16_t ADSD3500_STATUS_INVALID_CCB = 0x0007;
+    const uint16_t ADSD3500_STATUS_INVALID_CCB = 0x0007;
     /**
      * @brief Flash update error.
      */
-    static const uint16_t ADSD3500_STATUS_FLASH_HEADER_PARSE_ERROR = 0x0008;
+    const uint16_t ADSD3500_STATUS_FLASH_HEADER_PARSE_ERROR = 0x0008;
     /**
      * @brief Flash update error.
      */
-    static const uint16_t ADSD3500_STATUS_FLASH_FILE_PARSE_ERROR = 0x0009;
+    const uint16_t ADSD3500_STATUS_FLASH_FILE_PARSE_ERROR = 0x0009;
     /**
      * @brief SPI Master error occured, which this impacts the ADSD3500 - image communication.
      */
-    static const uint16_t ADSD3500_STATUS_SPIM_ERROR = 0x000A;
+    const uint16_t ADSD3500_STATUS_SPIM_ERROR = 0x000A;
     /**
      * @brief The imager chip ID is invalid.
      */
-    static const uint16_t ADSD3500_STATUS_INVALID_CHIPID = 0x000B;
+    const uint16_t ADSD3500_STATUS_INVALID_CHIPID = 0x000B;
     /**
      * @brief SPI Master error occured during communication between the ASDSD3500 and the imager.    
      */
-    static const uint16_t ADSD3500_STATUS_IMAGER_COMMUNICATION_ERROR = 0x000C;
+    const uint16_t ADSD3500_STATUS_IMAGER_COMMUNICATION_ERROR = 0x000C;
     /**
      * @brief Unable to boot the imager.
      */
-    static const uint16_t ADSD3500_STATUS_IMAGER_BOOT_FAILURE = 0x000D;
+    const uint16_t ADSD3500_STATUS_IMAGER_BOOT_FAILURE = 0x000D;
     /**
      * @brief The imager reported an error.
      */
-    static const uint16_t ADSD3500_STATUS_IMAGER_ERROR = 0x0010;
+    const uint16_t ADSD3500_STATUS_IMAGER_ERROR = 0x0010;
     /**
      * @brief This is when timer is expired but ADSD3500 is not able to send out frame due to some error.
      */
-    static const uint16_t ADSD3500_STATUS_TIMEOUT_ERROR = 0x0011;
+    const uint16_t ADSD3500_STATUS_TIMEOUT_ERROR = 0x0011;
     /**
      * @brief Dynamic mode switching is being set, but it is not enabled.
      */
-    static const uint16_t ADSD3500_STATUS_DYNAMIC_MODE_SWITCHING_NOT_ENABLED =
-        0x0013;
+    const uint16_t ADSD3500_STATUS_DYNAMIC_MODE_SWITCHING_NOT_ENABLED = 0x0013;
     /**
      * @brief The selected dyanamic mode configuration is not valid.
      */
-    static const uint16_t ADSD3500_STATUS_INVALID_DYNAMIC_MODE_COMPOSITIONS =
-        0x0014;
+    const uint16_t ADSD3500_STATUS_INVALID_DYNAMIC_MODE_COMPOSITIONS = 0x0014;
     /**
      * @brief An incorrect phase invalid value specified.
      */
-    static const uint16_t ADSD3500_STATUS_INVALID_PHASE_INVALID_VALUE = 0x0015;
+    const uint16_t ADSD3500_STATUS_INVALID_PHASE_INVALID_VALUE = 0x0015;
 
     // ADSD3500 status codes read via the "Get Status" (0x0020) command
     /**
      * @brief Firmware update is complete.
      */
-    static const uint16_t ADSD3500_STATUS_FIRMWARE_UPDATE_COMPLETE = 0x000E;
+    const uint16_t ADSD3500_STATUS_FIRMWARE_UPDATE_COMPLETE = 0x000E;
     /**
      * @brief NVM update is complete.
      */
-    static const uint16_t ADSD3500_STATUS_NVM_WRITE_COMPLETE = 0x000F;
+    const uint16_t ADSD3500_STATUS_NVM_WRITE_COMPLETE = 0x000F;
 
     /**
      * @brief Invalid mode selection.
      * Imager error codes read via "Get Imager Error Code" (0x0038) command
      * Note, this only valid if "Get Status" (0x0020) reports a value of "ADSD3500_STATUS_IMAGER_ERROR" (0x0010)
      */
-    static const uint16_t ADSD3100_ERR_MODE_USECASE = 0x0001;
+    const uint16_t ADSD3100_ERR_MODE_USECASE = 0x0001;
     /**
      * @brief Invalid LD mode selection.
      * Imager error codes read via "Get Imager Error Code" (0x0038) command
      * Note, this only valid if "Get Status" (0x0020) reports a value of "ADSD3500_STATUS_IMAGER_ERROR" (0x0010)
      */
-    static const uint16_t ADSD3100_ERR_MODE_MODE_DRIVER = 0x0002;
+    const uint16_t ADSD3100_ERR_MODE_MODE_DRIVER = 0x0002;
     /**
      * @brief PLLLOCK error location 1.
      * Imager error codes read via "Get Imager Error Code" (0x0038) command
      * Note, this only valid if "Get Status" (0x0020) reports a value of "ADSD3500_STATUS_IMAGER_ERROR" (0x0010)
      */
-    static const uint16_t ADSD3100_ERR_PLLLOCK_LOCK1 = 0x0004;
+    const uint16_t ADSD3100_ERR_PLLLOCK_LOCK1 = 0x0004;
     /**
      * @brief PLLLOCK error location 2.
      * Imager error codes read via "Get Imager Error Code" (0x0038) command
      * Note, this only valid if "Get Status" (0x0020) reports a value of "ADSD3500_STATUS_IMAGER_ERROR" (0x0010)
      */
-    static const uint16_t ADSD3100_ERR_PLLLOCK_LOCK2 =
+    const uint16_t ADSD3100_ERR_PLLLOCK_LOCK2 =
         0x0008; // PLLLOCK error location 2.
-    /**
+                /**
      * @brief PLLLOCK error location 3.
      * Imager error codes read via "Get Imager Error Code" (0x0038) command
      * Note, this only valid if "Get Status" (0x0020) reports a value of "ADSD3500_STATUS_IMAGER_ERROR" (0x0010)
      */
-    static const uint16_t ADSD3100_ERR_PLLLOCK_LOCK3 =
+    const uint16_t ADSD3100_ERR_PLLLOCK_LOCK3 =
         0x000C; // PLLLOCK error location 3.
-    /**
+                /**
      * @brief Imager sensor overheat.
      * Imager error codes read via "Get Imager Error Code" (0x0038) command
      * Note, this only valid if "Get Status" (0x0020) reports a value of "ADSD3500_STATUS_IMAGER_ERROR" (0x0010)
      */
-    static const uint16_t ADSD3100_ERR_OVERHEAT_IMAGER = 0x0010;
+    const uint16_t ADSD3100_ERR_OVERHEAT_IMAGER = 0x0010;
     /**
      * @brief Laser driver overheat.
      * Imager error codes read via "Get Imager Error Code" (0x0038) command
      * Note, this only valid if "Get Status" (0x0020) reports a value of "ADSD3500_STATUS_IMAGER_ERROR" (0x0010)
      */
-    static const uint16_t ADSD3100_ERR_OVERHEAT_LD = 0x0020;
+    const uint16_t ADSD3100_ERR_OVERHEAT_LD = 0x0020;
     /**
      * @brief Laser driver invalid chip ID.
      * Imager error codes read via "Get Imager Error Code" (0x0038) command
      * Note, this only valid if "Get Status" (0x0020) reports a value of "ADSD3500_STATUS_IMAGER_ERROR" (0x0010)
      */
-    static const uint16_t ADSD3100_ERR_LASER_CHIPID = 0x0040;
+    const uint16_t ADSD3100_ERR_LASER_CHIPID = 0x0040;
     /**
      * @brief Corrupted laser driver data.
      * Imager error codes read via "Get Imager Error Code" (0x0038) command
      * Note, this only valid if "Get Status" (0x0020) reports a value of "ADSD3500_STATUS_IMAGER_ERROR" (0x0010)
      */
-    static const uint16_t ADSD3100_ERR_LASER_LPS = 0x0080;
+    const uint16_t ADSD3100_ERR_LASER_LPS = 0x0080;
     /**
      * @brief Laser diffuser problem.
      * Imager error codes read via "Get Imager Error Code" (0x0038) command
      * Note, this only valid if "Get Status" (0x0020) reports a value of "ADSD3500_STATUS_IMAGER_ERROR" (0x0010)
      */
-    static const uint16_t ADSD3100_ERR_LASER_NO_DIFFUSER = 0x0100;
+    const uint16_t ADSD3100_ERR_LASER_NO_DIFFUSER = 0x0100;
     /**
      * @brief Laser driver shorted to GND.
      * Imager error codes read via "Get Imager Error Code" (0x0038) command
      * Note, this only valid if "Get Status" (0x0020) reports a value of "ADSD3500_STATUS_IMAGER_ERROR" (0x0010)
      */
-    static const uint16_t ADSD3100_ERR_LASER_SHORT = 0x0140;
+    const uint16_t ADSD3100_ERR_LASER_SHORT = 0x0140;
     /**
      * @brief Laser driver duty cycle too large.
      * Imager error codes read via "Get Imager Error Code" (0x0038) command
      * Note, this only valid if "Get Status" (0x0020) reports a value of "ADSD3500_STATUS_IMAGER_ERROR" (0x0010)
      */
-    static const uint16_t ADSD3100_ERR_LVDS_HIGH_DC = 0x0180;
+    const uint16_t ADSD3100_ERR_LVDS_HIGH_DC = 0x0180;
     /**
      * @brief Laser driver active time too long.
      * Imager error codes read via "Get Imager Error Code" (0x0038) command
      * Note, this only valid if "Get Status" (0x0020) reports a value of "ADSD3500_STATUS_IMAGER_ERROR" (0x0010)
      */
-    static const uint16_t ADSD3100_ERR_LVDS_PULSE_LONG = 0x01C0;
+    const uint16_t ADSD3100_ERR_LVDS_PULSE_LONG = 0x01C0;
     /**
      * @brief Laser driver input open or short detected.
      * Imager error codes read via "Get Imager Error Code" (0x0038) command
      * Note, this only valid if "Get Status" (0x0020) reports a value of "ADSD3500_STATUS_IMAGER_ERROR" (0x0010)
      */
-    static const uint16_t ADSD3100_ERR_LVDS_OPEN_SHORT = 0x0200;
+    const uint16_t ADSD3100_ERR_LVDS_OPEN_SHORT = 0x0200;
     /**
      * @brief Laser driver enabled for too long of time.
      * Imager error codes read via "Get Imager Error Code" (0x0038) command
      * Note, this only valid if "Get Status" (0x0020) reports a value of "ADSD3500_STATUS_IMAGER_ERROR" (0x0010)
      */
-    static const uint16_t ADSD3100_ERR_LASER_LONG_LEN_ON = 0x0240;
+    const uint16_t ADSD3100_ERR_LASER_LONG_LEN_ON = 0x0240;
     /**
      * @brief Laser driver disabled for too short of time.
      * Imager error codes read via "Get Imager Error Code" (0x0038) command
      * Note, this only valid if "Get Status" (0x0020) reports a value of "ADSD3500_STATUS_IMAGER_ERROR" (0x0010)
      */
-    static const uint16_t ADSD3100_ERR_LASER_SHORT_LEN_OFF = 0x0280;
+    const uint16_t ADSD3100_ERR_LASER_SHORT_LEN_OFF = 0x0280;
     /**
      * @brief Laser driver corrupted data.
      * Imager error codes read via "Get Imager Error Code" (0x0038) command
      * Note, this only valid if "Get Status" (0x0020) reports a value of "ADSD3500_STATUS_IMAGER_ERROR" (0x0010)
      */
-    static const uint16_t ADSD3100_ERR_LASER_LPS_READ = 0x02C0;
+    const uint16_t ADSD3100_ERR_LASER_LPS_READ = 0x02C0;
     /**
      * @brief Laser driver supply too low.
      * Imager error codes read via "Get Imager Error Code" (0x0038) command
      * Note, this only valid if "Get Status" (0x0020) reports a value of "ADSD3500_STATUS_IMAGER_ERROR" (0x0010)
      */
-    static const uint16_t ADSD3100_ERR_LASER_VLD_LOW = 0x0300;
+    const uint16_t ADSD3100_ERR_LASER_VLD_LOW = 0x0300;
     /**
      * @brief Laser driver supply too high.
      * Imager error codes read via "Get Imager Error Code" (0x0038) command
      * Note, this only valid if "Get Status" (0x0020) reports a value of "ADSD3500_STATUS_IMAGER_ERROR" (0x0010)
      */
-    static const uint16_t ADSD3100_ERR_LASER_VLD_HIGH = 0x0340;
+    const uint16_t ADSD3100_ERR_LASER_VLD_HIGH = 0x0340;
 
   private:
     std::unordered_map<uint16_t, std::string> m_ADSD3500StatusLookup;
