af1b52b32037adf84fead833a3262a93fdc78070
Feng <Ziyan.Feng@analog.com>
2023-12-07 15:43:44 -0800

    to pass clang_format erros

--- sdk/common/adi/tofi/tofi_camera_intrinsics.h
+++ sdk/common/adi/tofi/tofi_camera_intrinsics.h
@@ -5,8 +5,8 @@
 #define TOFI_CAMERA_INTRINSICS_H
 
 #ifdef __cplusplus
-extern "C" {  // only need to export C interface if
-              // used by C++ source code
+extern "C" { // only need to export C interface if
+             // used by C++ source code
 #endif
 #define MAX_PATH_SIZE 512
 #define MAX_CHAR_SIZE 24
@@ -14,90 +14,89 @@ extern "C" {  // only need to export C interface if
 #define MAX_N_MODES 17
 // Structure for the camera intrinsic data
 typedef struct {
-  float fx;
-  float fy;
-  float cx;
-  float cy;
-  float codx;
-  float cody;
-  float k1;
-  float k2;
-  float k3;
-  float k4;
-  float k5;
-  float k6;
-  float p2;
-  float p1;
+    float fx;
+    float fy;
+    float cx;
+    float cy;
+    float codx;
+    float cody;
+    float k1;
+    float k2;
+    float k3;
+    float k4;
+    float k5;
+    float k6;
+    float p2;
+    float p1;
 } CameraIntrinsics;
-
 
 // Structure for the CCB data
 typedef struct {
-  int n_rows;
-  int n_cols;
-  uint8_t n_freqs;
-  uint8_t row_bin_factor;
-  uint8_t col_bin_factor;
-  uint16_t n_offset_rows;
-  uint16_t n_offset_cols;
-  uint16_t n_sensor_rows;
-  uint16_t n_sensor_cols;
-  uint8_t FreqIndex[MAX_N_FREQS];
-  uint16_t Freq[MAX_N_FREQS];
-  CameraIntrinsics camera_intrinsics;
+    int n_rows;
+    int n_cols;
+    uint8_t n_freqs;
+    uint8_t row_bin_factor;
+    uint8_t col_bin_factor;
+    uint16_t n_offset_rows;
+    uint16_t n_offset_cols;
+    uint16_t n_sensor_rows;
+    uint16_t n_sensor_cols;
+    uint8_t FreqIndex[MAX_N_FREQS];
+    uint16_t Freq[MAX_N_FREQS];
+    CameraIntrinsics camera_intrinsics;
 } TofiXYZDealiasData;
 typedef struct {
-  int depthComputeIspEnable;
-  int partialDepthEnable;
-  int interleavingEnable;
-  int bitsInPhaseOrDepth;
-  int bitsInAB;
-  int bitsInConf;
-  int phaseInvalid;
-  int withABFrame;
-  int littleEndian;
-  char inputFormat[MAX_CHAR_SIZE];
+    int depthComputeIspEnable;
+    int partialDepthEnable;
+    int interleavingEnable;
+    int bitsInPhaseOrDepth;
+    int bitsInAB;
+    int bitsInConf;
+    int phaseInvalid;
+    int withABFrame;
+    int littleEndian;
+    char inputFormat[MAX_CHAR_SIZE];
 } DepthComputeISPParams;
 typedef struct {
-  char inputFormat[MAX_CHAR_SIZE];
-  int deltaCompEnable;
-  int headerSize;
-  int tempCompEnabled;
+    char inputFormat[MAX_CHAR_SIZE];
+    int deltaCompEnable;
+    int headerSize;
+    int tempCompEnabled;
 } InputRawDataParams;
 typedef struct {
-  int jblf_apply_flag;
-  int jblf_window_size;
-  float jblf_gaussian_sigma;
-  float jblf_exponential_term;
-  float jblf_max_edge;
-  float jblf_ab_threshold;
-  int ab_filter_enable;
+    int jblf_apply_flag;
+    int jblf_window_size;
+    float jblf_gaussian_sigma;
+    float jblf_exponential_term;
+    float jblf_max_edge;
+    float jblf_ab_threshold;
+    int ab_filter_enable;
 } JBLFConfigParams;
 typedef struct {
-  float ab_thresh_min;
-  float ab_sum_thresh;
+    float ab_thresh_min;
+    float ab_sum_thresh;
 } ABThresholdsParams;
 typedef struct {
-  float conf_thresh;
-  float radial_thresh_min;
-  float radial_thresh_max;
+    float conf_thresh;
+    float radial_thresh_min;
+    float radial_thresh_max;
 } DepthRangeParams;
 typedef struct {
-  int xyz_enable;
-  int depth16_enable;
-  int ab_only_enable;
-  char cl_processor_path[MAX_PATH_SIZE];
+    int xyz_enable;
+    int depth16_enable;
+    int ab_only_enable;
+    char cl_processor_path[MAX_PATH_SIZE];
 } MiscellaneousParams;
 typedef enum {
-  DepthComputeISP,
-  InputRawData,
-  JBLFConfig,
-  ABThresholds,
-  DepthRangeThresholds,
-  Miscellaneous
+    DepthComputeISP,
+    InputRawData,
+    JBLFConfig,
+    ABThresholds,
+    DepthRangeThresholds,
+    Miscellaneous
 } INIParamsGroup;
 #ifdef __cplusplus
 }
 #endif
 
-#endif  // TOFI_CAMERA_INTRINSICS_H
+#endif // TOFI_CAMERA_INTRINSICS_H
