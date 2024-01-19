ca94af36b8aeb52a8ccfff86cf63ad71d230e3cd
Alexandru Lie <Alexandru.Lie@analog.com>
2024-01-19 13:08:18 +0200

    Formatted command parser
    Signed-off-by: Alexandru Lie <Alexandru.Lie@analog.com>

--- dependencies/adi/command_parser/command_parser.cpp
+++ dependencies/adi/command_parser/command_parser.cpp
@@ -51,12 +51,13 @@ void CommandParser::parseArguments(
                     m_command_vector.push_back(
                         {std::string(argv[i]).substr(0, contains_equal),
                          std::string(argv[i]).substr(contains_equal + 1)});
-                } else if(arg_number != argc - 1){
+                } else if (arg_number != argc - 1) {
                     m_command_vector.push_back(
                         {arg_position[j].first, argv[i + 1]});
                     i++;
                 } else {
-                    m_command_vector.push_back({arg_position[j].first, argv[i]});
+                    m_command_vector.push_back(
+                        {arg_position[j].first, argv[i]});
                 }
                 arg_number++;
                 mandatory = true;
