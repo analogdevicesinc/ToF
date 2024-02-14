78e489e2eba55564a5eeee80df08ba5712f33671
Alexandru Lie <Alexandru.Lie@analog.com>
2024-02-14 16:49:58 +0200

    Target: Changed getAvailableFrameTypes to return only mode names
    Signed-off-by: Alexandru Lie <Alexandru.Lie@analog.com>

--- sdk/src/connections/target/adsd3500_sensor.h
+++ sdk/src/connections/target/adsd3500_sensor.h
@@ -52,8 +52,8 @@ class Adsd3500Sensor : public aditof::DepthSensorInterface,
     virtual aditof::Status open() override;
     virtual aditof::Status start() override;
     virtual aditof::Status stop() override;
-    virtual aditof::Status getAvailableFrameTypes(
-        std::vector<aditof::DepthSensorFrameType> &types) override;
+    virtual aditof::Status
+    getAvailableFrameTypes(std::vector<std::string> &types) override;
     virtual aditof::Status
     setFrameType(const aditof::DepthSensorFrameType &type) override;
     virtual aditof::Status getFrame(uint16_t *buffer) override;
