6e473fdbd5fda1f076b32a78dc5073079bb2965b
Alexandru Lie <Alexandru.Lie@analog.com>
2024-03-06 12:29:51 +0200

    sdk/src/connections: started implementation for new tables
    Signed-off-by: Alexandru Lie <Alexandru.Lie@analog.com>

--- sdk/src/connections/target/adsd3500_sensor.cpp
+++ sdk/src/connections/target/adsd3500_sensor.cpp
@@ -447,7 +447,11 @@ aditof::Status Adsd3500Sensor::stop() {
 aditof::Status
 Adsd3500Sensor::getAvailableFrameTypes(std::vector<std::string> &types) {
     types.clear();
+<<<<<<< HEAD
     for (const auto &frameType : m_availableFrameTypes) {
+=======
+    for (const auto &frameType : availableFrameTypes) {
+>>>>>>> 08272dd2 (sdk/src/connections: started implementation for new tables)
         types.emplace_back(frameType.mode);
     }
     return aditof::Status::OK;
@@ -458,7 +462,11 @@ Adsd3500Sensor::getFrameTypeDetails(const std::string &frameName,
                                     aditof::DepthSensorFrameType &details) {
     using namespace aditof;
     Status status = Status::OK;
+<<<<<<< HEAD
     for (const auto &frameDetails : m_availableFrameTypes) {
+=======
+    for (const auto &frameDetails : availableFrameTypes) {
+>>>>>>> 08272dd2 (sdk/src/connections: started implementation for new tables)
         if (frameDetails.mode == frameName) {
             details = frameDetails;
             break;
