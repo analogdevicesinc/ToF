aea55b3f5e4aaefb02dd8e4028ec921140337ca2
SeptimiuVana <Vana.Septimiu@analog.com>
2024-03-12 13:41:00 +0200

    sdk/connections/target/adsd3500_sensor: initialize m_modeSelector object
    Signed-off-by: SeptimiuVana <Vana.Septimiu@analog.com>

--- sdk/src/connections/target/adsd3500_sensor.cpp
+++ sdk/src/connections/target/adsd3500_sensor.cpp
@@ -847,13 +847,13 @@ aditof::Status Adsd3500Sensor::setControl(const std::string &control,
     }
 
     if (control == "phaseDepthBits")
-        ModeInfo::getInstance()->setSensorPixelParam("bitsInDepth", value);
+        m_sensorConfiguration.setControl("bitsInDepth", value);
     if (control == "abBits")
-        ModeInfo::getInstance()->setSensorPixelParam("bitsInAb", value);
+        m_sensorConfiguration.setControl("bitsInAb", value);
     if (control == "confidenceBits")
-        ModeInfo::getInstance()->setSensorPixelParam("bitsInConf", value);
+        m_sensorConfiguration.setControl("bitsInConf", value);
     if (control == "inputFormat") {
-        ModeInfo::getInstance()->setSensorPixelParam("pixelFormat", value);
+        m_sensorConfiguration.setControl("pixelFormat", value);
         return Status::OK;
     }
     if (control == "netlinktest") {
@@ -1624,10 +1624,12 @@ aditof::Status Adsd3500Sensor::queryAdsd3500() {
             switch (imager_version) {
             case 1: {
                 m_implData->imagerType = SensorImagerType::IMAGER_ADSD3100;
+                m_sensorConfiguration.setControl("imagerType", "adsd3100");
                 break;
             }
             case 2: {
                 m_implData->imagerType = SensorImagerType::IMAGER_ADSD3030;
+                m_sensorConfiguration.setControl("imagerType", "adsd3030");
                 break;
             }
             default: {
@@ -1644,6 +1646,21 @@ aditof::Status Adsd3500Sensor::queryAdsd3500() {
         }
     }
 
+<<<<<<< HEAD
+=======
+    if (m_implData->imagerType == SensorImagerType::IMAGER_UNKNOWN) {
+        LOG(WARNING) << "Since the image type is unknown, fall back on compile "
+                        "flag to determine imager type";
+#ifdef ADSD3030 // TO DO: remove this fallback mechanism once we no longer support old firmwares that don't support command 0x32
+        m_implData->imagerType = SensorImagerType::IMAGER_ADSD3030;
+        m_sensorConfiguration.setControl("imagerType", "adsd3030");
+#else
+        m_implData->imagerType = SensorImagerType::IMAGER_ADSD3100;
+        m_sensorConfiguration.setControl("imagerType", "adsd3100");
+#endif
+    }
+
+>>>>>>> 965b8918 (sdk/connections/target/adsd3500_sensor: initialize m_modeSelector object)
     if (m_implData->ccbVersion != CCBVersion::CCB_UNKNOWN) {
         if (m_implData->ccbVersion == CCBVersion::CCB_VERSION0) {
             setControl("modeInfoVersion", "0");
