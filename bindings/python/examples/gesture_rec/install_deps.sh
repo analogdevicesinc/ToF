a5b6a2a450bc92dd14c9400851635868cd0edadc
rbudai <robert.budai@analog.com>
2024-04-12 11:00:13 +0300

    bingins/python/examples: add gesture recognition
    Adapting Rock-Paper-Scissor gesture based application for CMOS sensors with Python Opencv
    
    Signed-off-by: rbudai <robert.budai@analog.com>

---
+++ bindings/python/examples/gesture_rec/install_deps.sh
@@ -0,0 +1,8 @@
+
+#!/bin/bash
+
+sudo apt-get install -y python3-tk
+sudo apt-get install -y python3-matplotlib
+sudo apt-get install -y python3-skimage
+sudo apt-get install -y python3-pil.imagetk
+sudo apt-get install -y python3-pil
