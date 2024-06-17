d29591f7cec00f96cb9aa8f31eb280076a23549e
Srivatsan Ravichandran <srivatsan.ravichandran@analog.com>
2024-06-17 15:07:24 +0000

    Added minor code changes for example program

--- tools/adsd3500_sample_program/src/adsd3500_util.cpp
+++ tools/adsd3500_sample_program/src/adsd3500_util.cpp
@@ -16,7 +16,6 @@
 * See the License for the specific language governing permissions and
 * limitations under the License.*/
 
-#include "../crc32/crc.h"
 #include <cstdint>
 #include <fcntl.h>
 #include <linux/i2c-dev.h>
