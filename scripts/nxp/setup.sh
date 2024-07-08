1f9100d3730fa90f004fb8185c4455020deec5e6
Karnati <Naveen.Karnati@analog.com>
2024-07-08 17:26:23 +0530

    Bug fix to setup.sh
    Signed-off-by: Karnati <Naveen.Karnati@analog.com>

--- scripts/nxp/setup.sh
+++ scripts/nxp/setup.sh
@@ -130,9 +130,9 @@ setup() {
         build_and_install_websockets ${deps_dir}/libwebsockets ${deps_install_dir}/websockets
 
         if [ -d ../../../libs ]; then
-                CMAKE_OPTIONS="-DNXP=1 -DWITH_PYTHON=1 -DCMAKE_BUILD_TYPE=Release .."
+                CMAKE_OPTIONS="-DNXP=1 -DWITH_PYTHON=1 -DCMAKE_BUILD_TYPE=Release"
         else
-                CMAKE_OPTIONS="-DNXP=1 -DUSE_DEPTH_COMPUTE_OPENSOURCE=ON -DWITH_PYTHON=1"
+                CMAKE_OPTIONS="-DNXP=1 -DUSE_DEPTH_COMPUTE_OPENSOURCE=ON -DWITH_PYTHON=1 -DCMAKE_BUILD_TYPE=Release"
         fi
 
         PREFIX_PATH="${deps_install_dir}/glog;${deps_install_dir}/protobuf;${deps_install_dir}/websockets;"
