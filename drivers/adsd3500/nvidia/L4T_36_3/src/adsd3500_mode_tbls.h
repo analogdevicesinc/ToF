2b2dd2855f021d9569a73f3ab0b4936662fce719
Sivasubramaniyan Padmanaban <sivasubramaniyan.padmanaban@analog.com>
2024-07-17 00:32:45 +0530

    drivers: adsd3500: nvidia: L4T_36_3: add patches and source files for jetson orin nano developer kit support
    Signed-off-by: Sivasubramaniyan Padmanaban <sivasubramaniyan.padmanaban@analog.com>

---
+++ drivers/adsd3500/nvidia/L4T_36_3/src/adsd3500_mode_tbls.h
@@ -0,0 +1,43 @@
+#ifndef __ADSD3500_I2C_TABLES__
+#define __ADSD3500_I2C_TABLES__
+
+enum {
+    ADSD3500_MODE_256x320_30FPS,
+    ADSD3500_MODE_1280x320_30FPS,
+    ADSD3500_MODE_512x512_30FPS,
+    ADSD3500_MODE_2560x512_30FPS,
+    ADSD3500_MODE_512x640_30FPS,
+    ADSD3500_MODE_2048x640_30FPS,
+    ADSD3500_MODE_2560x640_30FPS,
+    ADSD3500_MODE_1024x320_30FPS,
+    ADSD3500_MODE_1024x1024_30FPS,
+    ADSD3500_MODE_2048x2560_30FPS,
+    ADSD3500_MODE_2048x3328_30FPS,
+    ADSD3500_MODE_1024x2304_30FPS,
+};
+
+static const int adsd3500_30fps[] = {
+    30,
+};
+
+/*
+ * WARNING: frmfmt ordering need to match mode definition in
+ * device tree!
+ */
+static const struct camera_common_frmfmt adsd3500_frmfmt[] = {
+    {{256, 320}, adsd3500_30fps, 1, 0, ADSD3500_MODE_256x320_30FPS},
+    {{1280, 320}, adsd3500_30fps, 1, 0, ADSD3500_MODE_1280x320_30FPS},
+    {{512, 512}, adsd3500_30fps, 1, 0, ADSD3500_MODE_512x512_30FPS},
+    {{2560, 512}, adsd3500_30fps, 1, 0, ADSD3500_MODE_2560x512_30FPS},
+    {{512, 640}, adsd3500_30fps, 1, 0, ADSD3500_MODE_512x640_30FPS},
+    {{2048, 640}, adsd3500_30fps, 1, 0, ADSD3500_MODE_2048x640_30FPS},
+    {{2560, 640}, adsd3500_30fps, 1, 0, ADSD3500_MODE_2560x640_30FPS},
+    {{1024, 320}, adsd3500_30fps, 1, 0, ADSD3500_MODE_1024x320_30FPS},
+    {{1024,1024}, adsd3500_30fps, 1, 0, ADSD3500_MODE_1024x1024_30FPS},
+    {{2048,2560}, adsd3500_30fps, 1, 0, ADSD3500_MODE_2048x2560_30FPS},
+    {{2048,3328}, adsd3500_30fps, 1, 0, ADSD3500_MODE_2048x3328_30FPS},
+    {{1024,2304}, adsd3500_30fps, 1, 0, ADSD3500_MODE_1024x2304_30FPS},
+    /* Add modes with no device tree support after below */
+};
+
+#endif /* __ADSD3500_I2C_TABLES__ */
