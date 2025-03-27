8524469db65e6205257d99373ab14a49843df3ec
Andre Straker <andre.straker@analog.com>
2025-03-27 09:36:40 -0400

    ctrl_app, data_collect and ToF GUI added.

--- scripts/nvidia/scripts/filelist.sh
+++ scripts/nvidia/scripts/filelist.sh
@@ -1,6 +1,5 @@
 # 📦 Define your pattern list: "relative_src_pattern,relative_dst_folder"
 COPY_PATTERNS=(
-    "$SRC_PREFIX/bindings/python/build/lib/aditofpython.cpython-310-aarch64-linux-gnu.so,$LIB_INSTALL_FOLDER/lib",
     # Python Examples
     "$SRC_PREFIX/bindings/python/examples/first_frame/*.py,$DST_PREFIX/python",
     "$SRC_PREFIX/bindings/python/examples/data_collect/*.py,$DST_PREFIX/python",
@@ -10,6 +9,18 @@ COPY_PATTERNS=(
     # C++ Examples
     "$SRC_PREFIX/examples/first-frame/first-frame,$DST_PREFIX/C++",
     "$SRC_PREFIX/examples/data_collect/data_collect,$DST_PREFIX/C++",
+    "$SRC_PREFIX/examples/tof-viewer/ADIToFGUI,$DST_PREFIX/C++",
+    "$SRC_PREFIX/examples/tof-viewer/tof-tools.config,$DST_PREFIX/C++",
+    # Tools
+    ## ctrl_app
+    "$SRC_PREFIX/tools/debug_apps/ctrl_app/ctrl_app,$DST_PREFIX/tools/ctrl_app",
+    "$SRC_PREFIX/tools/debug_apps/ctrl_app/infile.txt,$DST_PREFIX/tools/ctrl_app",
+    "$SRC_PREFIX/tools/debug_apps/ctrl_app/readme.md,$DST_PREFIX/tools/ctrl_app",
+    ## V4L2 Scripts
+    ### TODO
+    ## NVM Tools
+    ### TODO
     # Stage Library files
+    "$SRC_PREFIX/bindings/python/build/lib/aditofpython.cpython-310-aarch64-linux-gnu.so,$DST_PREFIX/python",
     "/opt/ADI-ADCAM/*,$BASE_DST_PREFIX/libs"
 )
