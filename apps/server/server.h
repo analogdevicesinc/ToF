8e65410500ad75a79592ff77bc6777021de74fa2
Subham Singh <Subham.Singh@analog.com>
2025-04-21 09:31:47 +0000

    Removed libwebsocket references
    Signed-off-by: Subham Singh <Subham.Singh@analog.com>

--- apps/server/server.h
+++ apps/server/server.h
@@ -31,7 +31,6 @@
  */
 #include <google/protobuf/io/coded_stream.h>
 #include <google/protobuf/io/zero_copy_stream_impl.h>
-// #include <libwebsockets.h>
 #include <zmq.hpp>
 
 #define RX_BUFFER_BYTES (20996420)
