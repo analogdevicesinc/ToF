dc3d44eec8ab1adbc5c9f29636e2b018e88acfda
Subham Singh <Subham.Singh@analog.com>
2025-04-25 16:00:35 +0530

    Removed libwebsocket from CI
    Signed-off-by: Subham Singh <Subham.Singh@analog.com>

--- ci/azure/windows-build/install_deps.ps1
+++ ci/azure/windows-build/install_deps.ps1
@@ -10,8 +10,11 @@ mkdir -p deps_installed/Debug/glog
 mkdir -p deps_installed/Release/protobuf
 mkdir -p deps_installed/Debug/protobuf 
 
-mkdir -p deps_installed/Release/websockets
-mkdir -p deps_installed/Debug/websockets
+mkdir -p deps_installed/Release/libzmq
+mkdir -p deps_installed/Debug/libzmq
+
+mkdir -p deps_installed/Release/cppzmq
+mkdir -p deps_installed/Debug/cppzmq
 
 #Install glog
 git clone --branch v0.6.0 --depth 1 https://github.com/google/glog
@@ -27,20 +30,37 @@ cd ../build_0_6_0_Debug
 cmake -DWITH_GFLAGS=off -DCMAKE_INSTALL_PREFIX="$local_path/deps_installed/Debug/glog" -G $GENERATOR ..
 cmake --build . --target install --config Debug -j 4
 
-#Install websockets
+#Install libzmq
 cd $local_path
-git clone --branch v3.2.3 --depth 1 https://github.com/warmcat/libwebsockets
-cd libwebsockets
-mkdir build_3_2_3_Release
-mkdir build_3_2_3_Debug
+git clone --branch v4.3.4 --depth 1 https://github.com/zeromq/libzmq.git
+cd libzmq
+mkdir build_4_3_4_Release
+mkdir build_4_3_4_Debug
 
-cd build_3_2_3_Release
-cmake -DLWS_WITH_SSL=OFF -DCMAKE_INSTALL_PREFIX="$local_path/deps_installed/Release/websockets" -G $GENERATOR ..
-cmake --build . --target install --config Release -j 4 
+cd build_4_3_4_Release
+cmake -DBUILD_STATIC=off -DBUILD_TESTS=off  -DCMAKE_INSTALL_PREFIX="$local_path/deps_installed/Release/libzmq" -G $GENERATOR ..
+cmake --build . --target install --config Release -j 4
 
-cd ../build_3_2_3_Debug
-cmake -DLWS_WITH_SSL=OFF -DCMAKE_INSTALL_PREFIX="$local_path/deps_installed/Debug/websockets" -G $GENERATOR ..
+cd ../build_4_3_4_Debug
+cmake -DBUILD_STATIC=off -DBUILD_TESTS=off  -DCMAKE_INSTALL_PREFIX="$local_path/deps_installed/Debug/libzmq" -G $GENERATOR ..
 cmake --build . --target install --config Debug -j 4
+
+
+#Install cppzmq
+cd $local_path
+git clone --branch v4.9.0 --depth 1 https://github.com/zeromq/cppzmq.git
+cd cppzmq
+mkdir build_4_9_0_Release
+mkdir build_4_9_0_Debug
+
+cd build_4_9_0_Release
+cmake -DCPPZMQ_BUILD_TESTS=off  -DCMAKE_INSTALL_PREFIX="$local_path/deps_installed/Release/libzmq/build_4_3_4" -G $GENERATOR ..
+cmake --build . --target install --config Release -j 4
+
+cd ../build_4_9_0_Debug
+cmake -DCPPZMQ_BUILD_TESTS=off  -DCMAKE_INSTALL_PREFIX="$local_path/deps_installed/Debug/libzmq/build_4_3_4" -G $GENERATOR ..
+cmake --build . --target install --config Debug -j 4
+
 
 cd $local_path
 git clone --branch v3.9.0 --depth 1 https://github.com/protocolbuffers/protobuf
