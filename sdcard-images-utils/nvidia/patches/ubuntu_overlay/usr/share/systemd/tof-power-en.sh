f6953252815052c2650285b4bab3b3639b7bf3f6
Sivasubramaniyan Padmanaban <sivasubramaniyan.padmanaban@analog.com>
2025-05-09 15:29:23 +0530

    sdcard-images-utils: nvidia: patches: ubuntu_overlay: add the dual adsd3500 support to the tof power sequence
    Signed-off-by: Sivasubramaniyan Padmanaban <sivasubramaniyan.padmanaban@analog.com>

--- sdcard-images-utils/nvidia/patches/ubuntu_overlay/usr/share/systemd/tof-power-en.sh
+++ sdcard-images-utils/nvidia/patches/ubuntu_overlay/usr/share/systemd/tof-power-en.sh
@@ -6,14 +6,14 @@ if [[ $MODULE == "adi_adsd3500_adsd3100" ]]; then
 	echo "Module name: $MODULE"
 	echo "export CAM0_PWDN and set direction as output"
 
-	# VAUX_DAC_EN
+	# export ADSD3500 reset Pin
 	if [ ! -d /sys/class/gpio/PH.06 ]
 	then
 	echo 397 > /sys/class/gpio/export
 	echo out > /sys/class/gpio/PH.06/direction
 	fi
 
-	#ADSD3500 Reset Pin
+	#pull ADSD3500 reset low
 	sudo echo 0 > /sys/class/gpio/PH.06/value
 
 	#Disable the supply voltage
@@ -66,8 +66,67 @@ if [[ $MODULE == "adi_adsd3500_adsd3100" ]]; then
 	#EN_0P8
 	sudo gpioset 3 7=1
 
-	# Pull reset high
+	#Pull ADSD3500 reset high
 	sudo echo 1 > /sys/class/gpio/PH.06/value
+	echo "ToF power sequence completed"
+fi
+
+if [[ $MODULE == "adi_dual_adsd3500_adsd3100" ]]; then
+	echo "Module name: $MODULE"
+	echo "export CAM0_PWDN and set direction as output"
+
+	# export ADSD3500 reset Pin
+	if [ ! -d /sys/class/gpio/PH.06 ]
+	then
+		echo 397 > /sys/class/gpio/export
+		echo out > /sys/class/gpio/PH.06/direction
+	fi
+
+	#Pull ADSD3500 reset low
+	sudo echo 0 > /sys/class/gpio/PH.06/value
+
+	#EN_1P8
+	sudo gpioset 2 0=1
+
+	#EN_0P8
+	sudo gpioset 2 1=1
+
+	#I2CM_SET
+	sudo gpioset 2 3=1
+
+	#NET HOST_IO_SEL
+	sudo gpioset 2 5=1
+
+	#ISP_BS0
+	sudo gpioset 2 6=0
+
+	#ISP_BS1
+	sudo gpioset 2 7=0
+
+	#HOST_IO_DIR
+	sudo gpioset 2 8=0
+
+	#ISP_BS4
+	sudo gpioset 2 9=0
+
+	#ISP_BS5
+	sudo gpioset 2 10=0
+
+	#FSYNC_DIR
+	sudo gpioset 2 11=1
+
+	#EN_VAUX
+	sudo gpioset 2 12=1
+
+	#EN_VAUX_LS
+	sudo gpioset 2 13=1
+
+	#EN_VSYS
+	sudo gpioset 2 14=1
+
+	#Pull ADSD3500 reset high
+	sudo echo 1 > /sys/class/gpio/PH.06/value
+
 	echo "ToF power sequence completed"
 fi
 
