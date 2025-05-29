6525cc5c73af8ede4fac0a4d117d4baa5315d7ea
Subham Singh <Subham.Singh@analog.com>
2025-05-29 14:35:13 +0530

    update the code for web-ui
    Signed-off-by: Subham Singh <Subham.Singh@analog.com>

--- sdcard-images-utils/nxp/patches/ubuntu_overlay/step3/home/analog/Workspace/Tools/adi-enable-wifi.sh
+++ sdcard-images-utils/nxp/patches/ubuntu_overlay/step3/home/analog/Workspace/Tools/adi-enable-wifi.sh
@@ -1,7 +1,6 @@
 if [ "$#" -eq 2 ]; then
   wpa_passphrase $1 $2  | sudo tee /etc/wpa_supplicant/wpa_supplicant-wlan0.conf
   sudo systemctl enable wpa_supplicant@wlan0
-  sudo reboot
 fi
 
 echo "$0 <network SSID> <WiFi password>"
