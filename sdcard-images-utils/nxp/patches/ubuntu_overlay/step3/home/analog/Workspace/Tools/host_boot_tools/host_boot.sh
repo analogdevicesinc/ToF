3165f8b1d02a4b2576ae391e3e7b5cd89efb0f97
Sivasubramaniyan Padmanaban <sivasubramaniyan.padmanaban@analog.com>
2025-06-05 13:05:36 +0530

    Revert "sdcard-images-utils: nxp: patches: ubuntu_overlay: Add script to host and self boot dual adsd3500"
    This reverts commit 5831363bfd0214d374d3fcdb08b4902a8f65f6fd.

--- sdcard-images-utils/nxp/patches/ubuntu_overlay/step3/home/analog/Workspace/Tools/host_boot_tools/host_boot.sh
+++ sdcard-images-utils/nxp/patches/ubuntu_overlay/step3/home/analog/Workspace/Tools/host_boot_tools/host_boot.sh
@@ -1,251 +1,61 @@
 #!/bin/bash
 
+# Remove adsd driver
+sudo rmmod imx8_media_dev
+sudo rmmod adsd3500
+
 BOARD=$(strings /proc/device-tree/model)
 
-check_host_boot_pin_state() {
-
-	PIN_STATE=$(sudo cat /sys/class/gpio/gpio139/value)
-
-	echo -e "Boot pin state is $PIN_STATE"
-
-	if [ $PIN_STATE == 1 ]; then
-		echo -e "Host boot is enabled\n"
-	else
-		echo -e "Self boot is enabled\n"
-	fi
-}
-
-detect_i2c_devices(){
-
-	counter=0
-
-	echo "Detect I2C device"
-	while [[ $(i2cdetect -y 1 | grep 38) == "" ]]
-	do
-		sleep 1
-		((counter++))
-		if [ $counter -gt 30 ]; then
-			echo "Device not found and host boot failed"
-			exit 1
-		fi
-	done
-}
-
-remove_adsd3500_i2c_driver() {
-
-	sudo rmmod imx8_media_dev
-	sudo rmmod adsd3500
-}
-
-probe_adsd3500_i2c_driver() {
-
-	echo "Started ADSD3500 I2C host boot"
-	sudo modprobe adsd3500 fw_load=1
-	sudo modprobe imx8_media_dev
-	sleep 1
-	echo "Completed"
-}
-
-remove_adsd3500_spi_driver() {
-
-	sudo rmmod imx8_media_dev
-	sudo rmmod adsd3500-spi
-}
-
-probe_adsd3500_spi_driver() {
-
-	echo "Started ADSD3500 SPI host boot"
-	sudo modprobe adsd3500-spi fw_load=1
-	sudo modprobe imx8_media_dev
-	sleep 1
-	echo "Completed"
-}
-
-remove_adsd3500_dual_i2c_driver() {
-
-	sudo rmmod imx8_media_dev
-	sudo rmmod adsd3500_dual
-}
-
-probe_adsd3500_dual_i2c_driver() {
-
-	echo "Started ADSD3500 I2C host boot"
-	sudo modprobe adsd3500_dual fw_load=1 dyndbg==pmf
-	sudo modprobe imx8_media_dev
-	sleep 1
-	echo "Completed"
-}
-
-
-adsd3100_power_sequence(){
-
-	#ADSD3500 Reset Pin
-	echo 0 > /sys/class/gpio/gpio122/value
-
-	#EN_1P8
-	echo 0 > /sys/class/gpio/gpio510/value
-
-	sleep 1
-
-	#EN_0P8
-	echo 0 > /sys/class/gpio/gpio511/value
-
-	#BS0 Set 0 - self boot and 1 - host boot
-	echo 1 > /sys/class/gpio/gpio139/value
-
-	#EN_1P8
-	echo 1 > /sys/class/gpio/gpio510/value
-
-	sleep 1
-
-	#EN_0P8
-	echo 1 > /sys/class/gpio/gpio511/value
-
-	# Pull reset high
-	echo 1 > /sys/class/gpio/gpio122/value
-
-}
-
-adsd3030_power_sequence(){
-
-	#ADSD3500 Reset Pin
-	echo 0 > /sys/class/gpio/gpio122/value
-
-	#U5	#EN_3V3
-	echo 0 > /sys/class/gpio/gpio504/value
-
-	#U5	#EN_1V2
-	echo 0 > /sys/class/gpio/gpio505/value
-
-	#U5	#EN_1V8
-	echo 0 > /sys/class/gpio/gpio506/value
-
-	sleep 1
-
-	#U5	#EN_VLDD
-	echo 0 > /sys/class/gpio/gpio507/value
-
-	#U5	#EN_0V8
-	echo 0 > /sys/class/gpio/gpio509/value
-
-	#BS0 Set 0 - self boot and 1 - host boot
-	echo 1 > /sys/class/gpio/gpio139/value
-
-	#U5	#EN_3V3
-	echo 1 > /sys/class/gpio/gpio504/value
-
-	#U5	#EN_1V2
-	echo 1 > /sys/class/gpio/gpio505/value
-
-	#U5	#EN_1V8
-	echo 1 > /sys/class/gpio/gpio506/value
-
-	sleep 1
-
-	#U5	#EN_VLDD
-	echo 1 > /sys/class/gpio/gpio507/value
-
-	#U5	#EN_0V8
-	echo 1 > /sys/class/gpio/gpio509/value
-
-	# Pull reset high
-	echo 1 > /sys/class/gpio/gpio122/value
-
-}
-
-adsd3500_dual_power_sequence(){
-
-	#Disable supply voltage
-	echo 0 > /sys/class/gpio/gpio128/value
-	echo 0 > /sys/class/gpio/gpio129/value
-
-	sleep 1
-
-	#EN_PWR
-	echo 1 > /sys/class/gpio/gpio128/value
-
-	#EN_PWR2
-	echo 1 > /sys/class/gpio/gpio129/value
-
-	#Enable MAX77857
-	sudo i2cset -y 1 0x66 0x14 0x20
-	sleep 0.1
-
-	#Enable MAX77542
-	sudo i2cset -y 1 0x63 0x10 0xF
-
-	sleep 1
-
-	#ADSD3500 Reset Pin
-	echo 0 > /sys/class/gpio/gpio64/value
-
-	#BS0 Set 0 - Self boot and 1 - Host boot
-	echo 1 > /sys/class/gpio/gpio139/value
-
-	sleep 1
-
-	# Pull reset high
-	echo 1 > /sys/class/gpio/gpio64/value
-
-}
-
-enable_host_boot(){
-
-	case $BOARD in
+case $BOARD in
 		"NXP i.MX8MPlus ADI TOF carrier + ADSD3500")
 			echo "Running on ADSD3500 + ADSD3100"
-			check_host_boot_pin_state
-			remove_adsd3500_i2c_driver
-			adsd3100_power_sequence
-			detect_i2c_devices
-			probe_adsd3500_i2c_driver
+			NVM_GPIO='gpio507'
 			;;
 		"NXP i.MX8MPlus ADI TOF carrier + ADSD3030")
 			echo "Running on ADSD3500 + ADSD3030"
-			check_host_boot_pin_state
-			remove_adsd3500_i2c_driver
-			adsd3030_power_sequence
-			detect_i2c_devices
-			probe_adsd3500_i2c_driver
-			;;
-		"NXP i.MX8MPlus ADI TOF carrier ADSD3500-SPI + ADSD3100")
-			echo "Running on ADSD3500-SPI + ADSD3100"
-			check_host_boot_pin_state
-			remove_adsd3500_spi_driver
-			adsd3100_power_sequence
-			probe_adsd3500_spi_driver
-			;;
-		"NXP i.MX8MPlus ADI TOF carrier ADSD3500-SPI + ADSD3030")
-			echo "Running on ADSD3500-SPI + ADSD3030"
-			check_host_boot_pin_state
-			remove_adsd3500_spi_driver
-			adsd3030_power_sequence
-			probe_adsd3500_spi_driver
-			;;
-		"NXP i.MX8MPlus ADI TOF carrier ADSD3500-DUAL + ADSD3100")
-			echo "Running on ADSD3500-DUAL + ADSD3100"
-			check_host_boot_pin_state
-			remove_adsd3500_dual_i2c_driver
-			adsd3500_dual_power_sequence
-			detect_i2c_devices
-			probe_adsd3500_dual_i2c_driver
+			NVM_GPIO='gpio492'
 			;;
 		*)
 			echo "Board model not valid"
 			exit 1
 			;;
-	esac
-}
+esac
 
-main(){
+# Make i2c-* devices accessible by normal user
+sudo chmod a+rw /dev/i2c-*
 
-	enable_host_boot
-}
+# Select interposer Flash memory to force hostboot
+echo 1 | sudo tee /sys/class/gpio/$NVM_GPIO/value > /dev/null
 
-if [[ $EUID > 0 ]]; then
-                echo "This script must be run as root user"
-		echo "Usage: sudo ./host_boot.sh"
-		exit
-fi
+# Reset ADSD3500
+echo "Reset ADSD3500"
+echo 0 | sudo tee /sys/class/gpio/gpio122/value > /dev/null
+sleep 0.5
+echo 1 | sudo tee /sys/class/gpio/gpio122/value > /dev/null
 
-main
+# Wait for ADSD3500 to boot
+echo "Waiting for ADSD3500 to boot"
+echo "It can take up to 30 seconds"
+
+while [[ $(i2cdetect -y 1 | grep 38) == "" ]]
+do
+	sleep 1
+done
+#Install smbus2 module
+pip install smbus2-0.4.2-py2.py3-none-any.whl > /dev/null 2>&1
+
+
+# Run hostboot script
+python3 host_boot.py host_boot.stream
+
+# Select back the imager flash memory
+echo 0 | sudo tee /sys/class/gpio/$NVM_GPIO/value > /dev/null
+
+sleep 2
+
+# Read chip ID to check correct hostboot
+python3 read_chip_id.py
+
+# Reload ADSD3500 driver
+sudo modprobe adsd3500
+sudo modprobe imx8_media_dev
